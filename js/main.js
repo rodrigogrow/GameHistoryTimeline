bf44e35725fa6cd71e56f8a7067a811435196f63
Rodrigo Braga <rodrigogrow@gmail.com>
2015-03-15 23:02:09 -0400

    organize HTML

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 // when page was load all resources.
 $(document).ready(function(){
 
+	// startup timeliner
+	$.timeliner({timelineContainer:'#timeline'});
+	$('.venobox').venobox();
+
 	// array of content
 	var content = [_content['games_1950']];
 

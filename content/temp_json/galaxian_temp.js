d44476e7221b6a4fbd27d519321ecbeeb100f357
lukebrag <l2klucas@gmail.com>
2015-03-19 20:32:35 -0400

    Update galaxian_temp.js

--- content/temp_json/galaxian_temp.js
+++ content/temp_json/galaxian_temp.js
@@ -4,25 +4,38 @@
 	'authors': ['Kazunori Swano', 'Koichi Tashiro', 'Shigekazu Ishimura'],
 	'company': ['Namco'],
 	'year': '1979',
-	'date': 'Outubro de 1979',
-	'headline': 'A evolução de Space Invaders',
+	'date': 'October of 1979',
+	'headline': 'The evolution of the Space Invaders',
 	'resume': [
-		'Um jogo arcade segue a formula de Space Invaders, com um enredo ',
-		'simples e bastante objetivo de eliminar uma frota alienígena de ',
-		'uma raça chamada GALAXIAN.'
+		'Galaxian expanded on the formula pioneered by Space Invaders. ',
+		'As in the earlier game, Galaxian features a horde of attacking aliens ',
+		'that exchanged shots with the player.'
 	].join(''),
 	'full_text': [
-		'Sendo desenvolvido pela Namco em 1979 e lançado no mesmo no Japão,',
-		' sua jogabilidade consistia em atirar em uma frota de aliens que ',
-		'tentam periodicamente ataques kamikazes, os quais o jogador pode ',
-		'desviar se movendo pela vertical. Ademais, sua principal diferença',
-		'de sua inspiração é o fato de ser colorido na palheta RGB e ter uma',
-		' trilha sonora de fundo, além dos ataques Kamikazes.'
+		'An arcade game that was developed by Namco and released in October 1979. ',
+		'Swarm after swarm of alien armies attack the player's ship that move ',
+		'left and right at the bottom of the wraparound screen. The ship can only ',
+		'have one shot on screen at a time. The player defeats one swarm, ',
+		'only to have it replaced by another more aggressive and challenging swarm ',
+		'in the next stage. A plain and repetitive starfield scrolls in the background.'
 	].join(''),
-	'thumbnail': 'https://lh3.googleusercontent.com/E-kExiepZZHjWKBdm1LnIsQkdGocHz2YQGNJJGhPMYkX7_JXoLdJqcNpSeh1WMrWuf73AgYCsLGs_8A=w1416-h635',
-	'main_img': 'https://lh5.googleusercontent.com/6-8kHc527tsXkDGy2crMll7owCRIAKr-KLX2tlRWm6hyBCIh2pbrTcknMMt_TQVIysapoJ3pj_-TWXk=w1416-h679',
-	'images': [''],
-	'video_gameplay': ['https://www.youtube.com/watch?v=qKm8UnFCmeg'],
-	'demos': ['http://www.letsplaysnes.com/play-galaxian-online-nes/?play=true#content'],
-	'references': ['http://en.wikipedia.org/wiki/galaxian']
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=qKm8UnFCmeg', 'caption': "gameplay", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'http://www.letsplaysnes.com/play-galaxian-online-nes/?play=true#content', 'caption': "demo"}
+        ],
+        'references': [
+            {'url': 'http://en.wikipedia.org/wiki/galaxian', 'caption': "Wikipedia"},
+            {'url': 'reference_url2', 'caption': "description"},
+            {'url': 'reference_url3', 'caption': "description"}
+        ]
+}
 },

54767a875e93e7978093e1259780b0a18541f310
TeusMa <ma.santos.s@hotmail.com>
2015-03-19 21:11:08 -0300

    Create DeathRace_temp.js

---
+++ content/temp_json/DeathRace_temp.js
@@ -0,0 +1,36 @@
+{
+        'type': 'arcade ', // game or console
+        'name': 'DeathRace',
+        'authors': ['Exidy  . Designed by : Howell Ivy. ', 
+        'company': ['Exidy'],  
+        'year': '1976',
+        'date': '1976', // Day, Month, Year
+        'headline': 'um dos Primeiros jogos a causar uma grande polemica ',
+        'resume': 
+            'Você sabia, que o jogo Death Race Foi “culpado” Por causar Alguns dos vários Problemas No Mundo?, ',
+            'Para um jogo simples onde a única finalidade era atropelar Gremlins No começo parecia uma , ',
+            'ideia genial, o único problema era que os vulgos “gremlins” pareciam muito com Humanos , ',
+            'Como o gráfico era muito primitivo essa associação era muito “comum”   Claro que isso não , ',
+            'pegou muito bem Pois quando um gremlin era morto , Aparecia uma lapide no lugar assim , ', 
+            'Deixando a ligação entre Gremlin – Humano '
+        ].join(''),
+        'full_text': [
+            'O jogo em si, Pedestrian foi inspirado em um filme que se chamava Death Race 2000.  Ao passar do , ',
+            'tempo em que o jogo era desenvolvido sua produtora Exidy decidiu adotar o nome Death Race e assim , ',
+            'esquecendo o pedestrian, e batizando o jogo como Death Race, ',
+            'Assim como uma ilustração da Morte no panfleto promocional do game ou os esqueletos nos lados do , ',
+            'arcade do game. Num mundo onde videogames eram apenas uns bloquinhos coloridos feitos para  ',
+            'crianças, esse era um jogo muito adulto E assim claro a mídia que na época era muito conservadora não  ',
+            'gostou muito da ideia, claro Com a mídia muito agitada Surgiu várias reportagens mostrando o perigo e, ',
+            'um impacto psicológico que o game causava na mente de seus jogadores. ',
+            'E com isso um ex funcionário da Exidy afirmou que nessa polemica sobre o jogo fez que suas vendas , ',
+            'Triplicassem o Próprio presidente afirmou que quanto mais controvérsia existia  mais suas vendas aumentavam, ',
+            'A Exidy já faliu a muitos e muitos anos, mas sua franquia de death race Sobreviveu por muito tempo  o, ' ,
+            'game foi relançado para NES e mais de 20 anos depois Ganhou sua versão para PCs .'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png'
+        'main_img': 'DeathRaceArcade.jpeg',
+        'images': ['DeathRaceTela.jpeg', 'DeathRace3.jpeg', 'picture3.png'],
+        'video_gameplay': ['https://www.youtube.com/watch?v=aBBtt72aJLA'
+        'demos': ['demo_url1'],
+        'references': ['http://www.kotaku.com.br/', 'http://omelete.uol.com.br/']

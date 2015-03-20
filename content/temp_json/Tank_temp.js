a27fdc804bb9c5a6d7dc7f69822db353adc878d6
marcosdacio <marcosdacio@hotmail.com>
2015-03-19 21:48:33 -0300

    Update Tank_temp.js

--- content/temp_json/Tank_temp.js
+++ content/temp_json/Tank_temp.js
@@ -18,8 +18,10 @@
             'montado na parte superior do joysticks direito de cada par.',
             'Assim que inserir a moeda imediatamente o jogo é ligado, os jogadores ficam no canto superior direito  ',
             'e no canto inferior esquedo respectivamente.Tank foi o primeiro jogo a usar uma ROM baseada no IC para armazenar dados gráficos. ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu ',
-            'convallis. Volutpat blandit egestas et libero eu per, venenatis ',
+            'Os jogadores movem evitando minas e atirando um no outro, os jogadores são representados por um sprite preto e branco',
+            'de taque e as minas são representandos por um"X".Os pontos ão marcados por tiros ao adversário ou quando seu adversári',
+            'passa sobre uma mina,o jogador com maior pontuação no final do prazo de tempo,ganha o jogo.'
+         
             'aenean, proin quis aliquam viverra potenti vestibulum auctor, ',
             'non amet tellus sodales. In ullamco tellus risus nisl morbi amet. ',
             'Vel quisque nunc in placerat. Vitae id urna, nulla suspendisse, ',

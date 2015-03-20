23a557ee7e38f630ac8dc70638d1308ebffa05ee
lukebrag <l2klucas@gmail.com>
2015-03-19 20:47:32 -0400

    Create galaxianpt_temp.js

---
+++ content/temp_json/galaxianpt_temp.js
@@ -0,0 +1,41 @@
+{
+	'type': 'game', // game or console
+	'name': 'Galaxian',
+	'authors': ['Kazunori Swano', 'Koichi Tashiro', 'Shigekazu Ishimura'],
+	'company': ['Namco'],
+	'year': '1979',
+	'date': 'October of 1979',
+	'headline': 'The evolution of the Space Invaders',
+	'resume': [
+		'Um jogo arcade segue a formula de Space Invaders, com um enredo ',
+		'simples e bastante objetivo de eliminar uma froto alienígena de ',
+		'uma raça chamada GALAXIAN.'
+	].join(''),
+	'full_text': [
+		'Sendo desenvolvido pela Namco em 1979 e lançado no mesmo no Japão,',
+		' sua jogabilidade consistia em atirar em uma frota de aliens que ',
+		'tentam periodicamente ataques kamikazes, os quais o jogador pode ',
+		'desviar se movendo pela vertical. Ademais, sua principal diferença',
+		' de sua inspiração é o fato de ser colorido na palheta RGB e ter',
+		' uma trilha sonora de fundo, além dos ataques Kamikazes.'
+	].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=qKm8UnFCmeg', 'caption': "gameplay", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'http://www.letsplaysnes.com/play-galaxian-online-nes/?play=true#content', 'caption': "demo"}
+        ],
+        'references': [
+            {'url': 'http://en.wikipedia.org/wiki/galaxian', 'caption': "Wikipedia"},
+            {'url': 'reference_url2', 'caption': "description"},
+            {'url': 'reference_url3', 'caption': "description"}
+        ]
+}
+},

e58376b37da0ed505dfeccd25b819c97e2be20ae
BrazilGabriel <gabriel.br1994@gmail.com>
2015-03-21 14:47:37 -0400

    Update computer_space_temp.js

--- content/temp_json/computer_space_temp.js
+++ content/temp_json/computer_space_temp.js
@@ -1,10 +1,10 @@
 {
-        'type': 'game', // game or console
+        'type': 'console', // game or console
         'name': 'Computer Space',
-        'authors': ['Nolan Bushnell', 'Ted Dabney'],
+        'authors': ['Nolan K. Bushnell', 'Ted Dabney'],
         'company': ['Nutting Associates'],
         'year': '1971',
-        'date': '20-06-1951', // Day, Month, Year
+        'date': '15-10-1971', // Day, Month, Year
         'headline': 'First Game about sport',
         'resume': [
             'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
@@ -14,17 +14,11 @@
             'etiam tempus wisi semper montes odio, lorem vitae vel eu '
         ].join(''),
         'full_text': [
-            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
-            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu ',
-            'convallis. Volutpat blandit egestas et libero eu per, venenatis ',
-            'aenean, proin quis aliquam viverra potenti vestibulum auctor, ',
-            'non amet tellus sodales. In ullamco tellus risus nisl morbi amet. ',
-            'Vel quisque nunc in placerat. Vitae id urna, nulla suspendisse, ',
-            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
-            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+            'Em 1971  os Estados Unidos da América começavam a retirar suas tropas do Vietnã e a geração paz e amor aos poucos dava espaço para o pessoal da discoteca.',
+            'Em meio a este cenário de transição do protesto para a era da tecnologia e eletrônica, Nolan K. Bushnell, o então engenheiro-chefe da  Nutting Associates em parceiria com Ted Dabney , lança o Arcade Computer Space.',
+            'Computer Space tem clara inspiração no game SpaceWar e foi o primeiro arcade produzido em grande escala.',
+            'O game consiste em eliminar adversários que se assemelham a um disco voador; para isso você comanda uma espaçonave parecida com um ônibus espacial e que poderia ser tribulado pelos Thunderbirds.',
+            'Bushnell transformou o quarto da sua filha em uma oficina para poder criar uma máquina que devido ao espaço que os computadores da época ocupavam, serviria apenas para jogar o SpaceWar!, e essa máquina foi chamada de Computer Space. O Computer Space foi o primeiro arcade do mundo e também o primeiro jogo eletrônico a ser comercializado no mundo. Como a experiência deu certo, a Nutting Associates contrata Bushnell temporariamente para montar os arcades Computer Space. Mais tarde Bushnell sai da empresa e funda a Atari.'
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},
@@ -34,14 +28,14 @@
             {'name': 'picture3.png', 'caption': "description3"}
         ],
         'video_gameplay': [
-            {'url': 'video_url', 'caption': "description", 'type': "youtube"} // type: youtube or vimeo
+            {'url': 'https://www.youtube.com/watch?feature=player_embedded&v=Nhdu2Jh9cuc', 'caption': "Computer Space gameplay", 'type': "youtube"} // type: youtube or vimeo
         ],
         'demos': [
             {'url': 'demonstration_url', 'caption': "description"}
         ],
         'references': [
             {'url': 'http://www.technologizer.com/2011/12/11/computer-space-and-the-dawn-of-the-arcade-video-game/', 'caption': "Computer Space and the Dawn of the Arcade Video Game"},
-            {'url': 'reference_url2', 'caption': "description"},
-            {'url': 'reference_url3', 'caption': "description"}
+            {'url': 'http://blablagames.net/?p=910', 'caption': "BLA…Bla…Games"},
+            {'url': 'https://gamesample.wordpress.com/tag/computer-space/', 'caption': "A historia dos jogos eletrônicos parte 2 – O nascimento dos Arcades e consoles"}
         ]
 }

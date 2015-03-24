e97a04f5a967352e057f8a7c43478309296f1390
Gabriel014 <formmo@live.com>
2015-03-24 13:48:06 -0400

    Create Nimrod_temp.js

---
+++ content/temp_json/Nimrod_temp.js
@@ -0,0 +1,53 @@
+{
+		        'type': 'game', // game or console
+		        'name': 'NIMROD',
+		        'authors': ['Edward Uhler Condon'],
+		        'company': ['Ferranti'],  
+		        'year': '1951',
+		        'date': '05-05-1951', // Day, Month, Year
+		        'headline': 'First Electronic Game',
+		        'resume': [
+		            'Usando um painel de luzes para a sua exibição, ',
+					'ele foi projetado exclusivamente para jogar o jogo de Nim. ',
+					'Este foi o primeiro exemplo de um computador digital projetado ',
+					'especificamente para jogar um jogo. ',
+					'O criador do NIMATRON foi Edward Uhler Condon.',
+					'A ideia do jogo era simples e já existia fora do mundo',
+					'tecnológico. O NIMROD usava painéis de luzes para simular',
+					'as peças usadas no já conhecido jogo Nim"
+        ].join(''),
+ 'full_text': [
+		            'Em 5 de maio de 1951, o computador NIMROD, criado pela Ferranti, ',
+					'foi apresentado no Festival da Grã-Bretanha. ',
+		            'A empresa era focada em segurança eletrônica e em seu tempo livre',
+		            'Edward Uhler Condon utilizou a ferramentas disponíveis e criou ',
+		            'o NIMATRON cujo foco era simular o jogo antigo NIM, antes jogado com pedras. ',
+		            'As pedras foram trocadas por um painel com luzes e o controle era um totem. ',
+		            'O objetivo do jogo era deixar o adversário, nesse caso o computador, ', 
+					'com a última peça. O jogador poderia remover quantas peças quisesse ',
+					'de uma linha, por vez. Só que em 90% das vezes o computador vencia.',
+					'O jogo fez um sucesso enorme nas feiras em que foi exposto, fazendo',
+					'com que muitas pessoas se aventurassem a jogá-lo. Em algumas exposições,',
+					'até pessoas importantes como embaixadores jogaram o jogo.',
+					'Em Berlim, as pessoas esquecerma da cerveja que estava sendo distribuída',
+					'gratuitamente no local para concentrar a atenção na exposição do jogo'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=yJZlozoQ4jI', 'caption': "Gameplay Nimrod", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'http://en.wikipedia.org/wiki/Nimrod_(computing), 'caption': "Wikipédia"},
+            {'url': 'http://www.wired.com/2010/06/replay/', 'caption': "Wired"},
+            {'url': 'http://www.historyofinformation.com/expanded.php?id=4471', 'caption': "History of Information"}
+        ]
+}

6751c101bdbb7701cd6431dddd44aaff74f2f74b
lukebrag <l2klucas@gmail.com>
2015-03-24 17:45:17 -0300

    Create TennisForTwoPT_temp.js

---
+++ content/temp_json/TennisForTwoPT_temp.js
@@ -0,0 +1,57 @@
+{
+        'type': 'game', // game or console
+        'name': 'Tennis For Two',
+        'authors': ['William Higinbotham'],
+        'company': ['Brookhaven National Laboratory'],  
+        'year': '1958,
+        'date': '18-10-1958', // Day, Month, Year
+        'headline': 'Um dos primeiros jogos eletrônicos a usar um display gráfico',
+        'resume': [
+            'Tennis for Two é frequentemente considerado como um dos ',
+            'primeiros jogos já criados. Desenvolvido por William ',
+            'Higginbotham, um físico que trabalhou no Projeto Manhattan, ',
+            'Tennis for Two foi completado em 18 de Outubro de 1958, ',
+            'muito antes de os primeiros jogos serem comercializados.',
+        ].join(''),
+        'full_text': [
+            'Tennis for Two é  considerado um dos primeiros jogos, ',
+            'já lançados, se não o primeiro. Está entre os primeiros ',
+            'jogos que contou com um display gráfico. Higginbotham ',
+            'criou o jogo em resposta ao "Dia de Exposições" no ',
+            'Laboratório Nacional de Brookhaven em Upton, Nova Iorque. ',
+            'De acordo com Higinbotham, o dia de exposições sempre era ',
+            'um tanto monótomo e entediante, e Tennis for Two foi criado ',
+            'em resposta a esse fato, para tornar o evento mais ',
+            'emocionante. Um dos computadores do exército era capaz de ',
+            'calcular a trajetória de mísseis balísticos, e ele usou ',
+            'essa tecnologia para desenvolver o jogo. Sem surpresa, o jogo ',
+            "foi um enorme sucesso entre os participantes e relatos sugerem ',
+            que enormes filas foram formadas para jogar o \'simples\', mas, ',
+            no momento, \'inovador\' jogo. Mesmo que Higginbotham pecebesse que ',
+            'tinha desenvolvido algo original, ele nunca patenteou a ',
+            'ideia devido ao fato de que o governo dos Estados Unidos ',
+            'teria tomado posse dela("Mas se eu tivesse percebido o ',
+            'quão significativo havia sido, eu teria patenteado ',
+            'e seria posse do governo dos EUA!" -Higginbotham). ',
+            'Durante dois anos, o jogo foi desenvolvido, até que fosse ',
+            'infelizmente, descontinuado.'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=s2E9iSQfGdg', 'caption': "Tennis For Two - The second ever computer game"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'http://videogamehistory.wikia.com/wiki/Tennis_for_Two', 'caption': "Video Game History Wiki"},
+            {'url': 'http://www.youtube.com/watch?v=6QSHZ20MQfE', 'caption': "When Games Went Click: The Story of Tennis for Two"},
+            {'url': 'http://www.bnl.gov/bnlweb/history/higinbotham.asp', 'caption': "When Games Went Click: The Story of Tennis for Two"}
+        ]
+}

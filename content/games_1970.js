b092a155f58ff1754628fdd5429476180fc3f8b3
Rodrigo Braga <rodrigogrow@gmail.com>
2015-03-23 23:56:51 -0400

    update pong

--- content/games_1970.js
+++ content/games_1970.js
@@ -10,32 +10,48 @@
 		'decade': '1970',
 		'games': [
 			{
-				'type': 'game', // game or console
-				'name': 'Pong',
-				'authors': ['Allan Alcorn'],
-				'company': ['Atari'],  
-				'year': '1972',
-				'date': '1972',
-				'headline': 'First Game about space',
-				'resume': [
-					'Um dos primeiros videogames chamava-se Pong e representava um jogo de tênis a duas ',
-					'dimensões e a preto e branco. O objetivo do jogo era derrotar os adversários obtendo uma ',
-					'pontuação superior a eles. '
-				 ].join(''),
-				'full_text': [
-				  	'Lançado em 1972 pela Atari, este foi sem dúvida o jogo que mais contribuiu para a explosão, ',
-					'da indústria dos videogames e foi também o primeiro jogo lucrativo da história dos ',
-					'games. Foi criado por Allan Alcorn. ',
-					'Alcorn conseguiu criar características inovadoras no Pong, tais como a variação do ângulo da ',
-					'bola dependendo da parte com esta era devolvida, o aumento gradual da velocidade do jogo e ',
-					'as variantes de sons do jogo.'
-				 ].join(''),
-				'thumbnail': '',
-		        'main_img': 'pong_img_1.png', 
-				'images': ['pong_img_1.png'],
-				'video_gameplay': [],
-				'demos': [],
-				'references': ['http://origemdascoisas.com/a-origem-do-pong']
+			    'type': 'game', // game or console
+			    'name': 'Pong',
+			    'authors': ['Allan Alcorn'],
+			    'company': ['Atari'],
+			    'year': '1972',
+			    'date': '01-11-1972', // Day, Month, Year
+			    'headline': 'Primeiro videogame lucrativo da História',
+			    'resume': [
+			        'Um dos primeiros videogames chamava-se Pong e representava um jogo',
+			        'de tênis para dois jogadores em uma televisão preto e branco. O objetivo ',
+			        'era derrotar os adversários obtendo uma pontuação superior a eles.',
+			    ].join(''),
+			    'full_text': [
+			        'Lançado em 1972 pela Atari, este foi sem dúvida o jogo que mais contribuiu',
+			        'para a explosão da indústria dos videogames e foi também o primeiro',
+			        'jogo lucrativo da história dos games. A sua criação deve-se a Allan',
+			        'Alcorn. Allan desenvolveu características inovadoras para o Pong,',
+			        'tais como a variação do ângulo da bola dependendo da parte como esta',
+			        'era devolvida; O aumento gradual da velocidade do jogo e as variações',
+			        'de sons. Para construir o protótipo e como necessitava de espaço para',
+			        'os componentes, Alcorn comprou uma televisão Hitachi preto e branco ',
+			        'e construiu uma cabine de madeira com um 1,20 mt de altura onde inseriu',
+			        'a televisão os restantes circuitos integrados necessários ao ',
+			        'funcionamento do jogo.'
+			    ].join(''),
+			    'thumbnail': 'http://ts3.mm.bing.net/th?id=HN.608005174475620362&pid=1.7',
+			    'main_img': {'name': 'pong_img_1' , 'caption': "Pong for Atari"},
+			    'images': [
+			        {'name': 'http://origemdascoisas.com/wp-content/uploads/2012/03/Maquina-de-Pong-1972-Pong-Machine-1972.jpg', 'caption': "Máquina de Pong em 1972"},
+			        {'name': 'http://ts1.mm.bing.net/th?id=HN.608054072673043784&pid=1.7', 'caption': "Versão doméstica do Pong"},
+			        {'name': 'picture3.png', 'caption': "description3"}
+			    ],
+			    'video_gameplay': [
+			        {'url': 'http://www.youtu.be/RKF-yoX3V4k', 'caption': "Jogabilidade do clássico Pong", 'type': "youtube"} // type: youtube or vimeo
+			    ],
+			    'demos': [
+			        {'url': 'https://atari.com/arcade#!/arcade/pong/play', 'caption': "Jogue agora!"}
+			    ],
+			    'references': [
+			        {'url': 'http://origemdascoisas.com/a-origem-do-pong/', 'caption': "A Origem das Coisas: A Origem do Pong"},
+			        {'url': 'http://pt.wikipedia.org/wiki/Pong', 'caption': "Pong"},
+			    ]
 			},
 			{
 			    'type': 'game', // Atari

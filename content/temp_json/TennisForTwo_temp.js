02cb2766d666a6516384e6b85a69ac181417fe34
lukebrag <l2klucas@gmail.com>
2015-03-24 16:50:43 -0300

    Create TennisForTwo_temp.js
    New and updated information about the game

---
+++ content/temp_json/TennisForTwo_temp.js
@@ -0,0 +1,57 @@
+{
+        'type': 'game', // game or console
+        'name': 'Tennis For Two',
+        'authors': ['William Higinbotham'],
+        'company': ['Brookhaven National Laboratory'],  
+        'year': '1958',
+        'date': '10-18-1958', // Day, Month, Year
+        'headline': 'One of the first electronic games to use a graphical display.',
+        'resume': [
+            'Tennis for Two is often regarded as one of the first video ',
+            'games ever created. Developed by William Higginbotham, ',
+            'a physicist who worked on the Manhattan Project, Tennis ',
+            'for Two was completed on October 18, 1958, long before the ',
+            'first commercial video games were ever released. '
+        ].join(''),
+        'full_text': [
+            'Tennis for Two is generally considered one of the earliest, ',
+            'if not the earliest, video gameseverreleased. It is among '
+			'the first video games that featured an graphical display. '
+			'Higginbotham created the game in response to the upcoming '
+			'"visitor day" at the Brookhaven National Laboratory in Upton, '
+			'New York. According to Higinbotham, the visitor day was '
+			'generally rather uneventful and boring, and Tennis for Two '
+			'was created in response to this fact to make the event more '
+			'exciting. One of the computers the army used was able to '
+			'calculate the trajectory of ballistic missiles, and he used '
+			'this technology to develop the game. Unsurprisingly, the game '
+			'was a huge hit among the attendees and reports suggest that '
+			'huge lines formed to play the simple, yet at the time '
+			'innovative game. Even though Higginbotham realized that he '
+			'had developed something very unique, he never patented the '
+			'idea due to the fact that the United States government '
+			'would have gained ownership of it("But if I had realized '
+			'just how significant it was, I would have taken out a patent '
+			'and the U.S. government would own it!" - Higginbotham). '
+			'For two years the game was in commission until it was '
+			'unfortunately dismanteled.'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=s2E9iSQfGdg', 'caption': "Tennis For Two - The second ever computer game", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'http://videogamehistory.wikia.com/wiki/Tennis_for_Two', 'caption': "Video Game History Wiki"},
+            {'url': 'http://www.youtube.com/watch?v=6QSHZ20MQfE', 'caption': "When Games Went Click: The Story of Tennis for Two"},
+            {'url': 'http://www.bnl.gov/bnlweb/history/higinbotham.asp', 'caption': "When Games Went Click: The Story of Tennis for Two"}
+        ]
+}

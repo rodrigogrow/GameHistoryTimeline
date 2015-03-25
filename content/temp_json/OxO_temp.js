ea66027394b3b46ca6c054d92d06327b0f41e60f
DKPagini <jameson.pagini@outlook.com>
2015-03-25 15:15:56 -0300

    Create OxO_temp.js

---
+++ content/temp_json/OxO_temp.js
@@ -0,0 +1,57 @@
+'name': 'OxO - Naughts and Crosses',
+        'authors': ['Shafto Douglas', 'Alexander'],
+        'year': '1952',
+        'headline': 'OxO - O primeiro jogo digital.',
+        'resume': [
+            'Foi em 1952 que surgiu o primeiro vídeo game, do jeito que deve ser, ',
+            'gráficos digitais exibidos em um monitor de tubo de raios catódicos., ',
+            'OXO, Noughts and Crosses foi desenvolvido por Alexander Shafto Douglas, ',
+            'como parte da sua tese de doutorado Interação Homem-Computador, ',
+            'O jogo tinha como controles um telefone analógico, onde cada número',
+            'discado representava o quadro onde seria feito o movimento. ',
+            'Apesar da simplicidade e da aparente falta de emoção',
+            'o jogo chamava atenção por representar bem a “Interação Homem-Computador”. ',
+            'Apesar de brilhante, OXO não se popularizou porque só funcionava ',
+            'no computador em que foi desenvolvido – o EDSAC – e ele era o único no mundo!',
+            'A.S Douglas ganhou seu título de doutor e OXO foi considerado o primeiro vídeo game'.
+        ].join(''),
+        'full_text': [
+            'Somente em 1952 foi que surgiu o primeiro vídeo game, do jeito que deve ser - gráficos digitais',
+            'exibidos em um monitor de tubo de raios catódicos. OXO, Noughts and Crosses foi desenvolvido',
+            'por Alexander Shafto Douglas como parte da sua tese de doutorado “Interação Homem-Computador”. ',
+            'OXO, também conhecido como "Noughts and Crosses" (Zeros e Cruzes), ',
+            'é um jogo da velha de computador feito para o computador EDSAC em 1952.',
+            'Já dentro dos quartéis, alguns militares aproveitavam o tédio para se divertir ',
+            ' e fazer alguma coisa nos computadores que só eles tinham. Surgiram alguns jogos, ',
+            'é verdade, mas eles eram analógicos e não podiam ser caracterizados como vídeo games ',
+            'porque ou não tinham gráficos, ou os que tinham não eram exibidos em um monitor, ',
+            'mas em um display vetorial.',
+            'O jogo na realidade trata-se nada mais nada menos do que o famoso ',
+            '“Jogo da Velha”, só que jogado em um computador com um controle de telefone analógico.',
+            'O esquema da jogatina é o mesmo desde sempre: posicione o “X” ou a “O” no lugar que ',
+            'você quer e feche uma linha com eles. Apesar da simplicidade e da aparente falta de ',
+            'emoção, o jogo chamava atenção justamente por representar muitíssimo bem a “Interação ',
+            'Homem-Computador”, coisa que pouca gente conhecia fora das universidades e recantos ',
+            'militares.',
+            'Apesar de brilhante, OXO não se popularizou porque só funcionava no computador  em  que ',
+            'foi desenvolvido – o EDSAC – e ele era o único no mundo! Mesmo não tendo ganhado nem  ',
+            'um centavo com o jogo, Alexander Shafto Douglas ganhou seu título de doutor e OXO foi considerado o ',
+            'primeiro vídeo game do mundo!'
+        ].join(''),
+        'thumbnail': 'oxo.jpg',
+        'main_img': {'name': '1952-OXO.png', 'caption': "Emulaçao do OXO"},
+        'images': [
+            {'name': '1952-OXO.png', 'caption': "Emulador do jogo"},
+            {'name': 'oxo.jpg', 'caption': "Digitalizaçao do OXO"},
+            {'name': 'edsac.jpg', 'caption': "Computador utilizado"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=Pkiwn7u8i8s', 'caption': "Gameplay do jogo OXO", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'references': [
+            {'url': 'http://en.wikipedia.org/wiki/OXO', 'caption': "Game history"},
+            {'url': 'http://contembits.com.br/historia/jogos/1952-OXO.aspx', 'caption': "História dos jogos eletrônicos"},
+            {'url': 'http://www.nintendoblast.com.br/2010/01/historia-dos-video-games-1-o-comeco-de.html', 'caption': "O começo dos vídeos games"}
+            {'url': 'http://www.muitointeressante.com.br/tags/oxo', 'caption': "História do oxo"}
+        ]
+}

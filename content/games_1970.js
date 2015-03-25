449a30c873172340e5b20f033065543994cbfc0b
Rodrigo Braga <rodrigogrow@gmail.com>
2015-03-24 21:07:22 -0400

    update project

--- content/games_1970.js
+++ content/games_1970.js
@@ -10,6 +10,45 @@
 		'decade': '1970',
 		'games': [
 			{
+		        'type': 'game', // game or console
+		        'name': 'Computer Space',
+		        'authors': ['Nolan K. Bushnell', 'Ted Dabney'],
+		        'company': ['Nutting Associates'],
+		        'year': '1971',
+		        'date': '15-10-1971', // Day, Month, Year
+		        'headline': 'First Arcade Game',
+		        'resume': [
+		            'Computer Space foi o primeiro jogo de fliperama lançado em 1971 pela Nutting Associates, criado por Nolan Bushnell e Ted Dabney, que funcionava com moedas ',
+		            'Computer Space foi inovador em estabelecer a forma básica de todos os fliperamas - a de um dispositivo de ',
+		            'computação dedicada construído para jogar apenas um jogo. Foi um referencial por ser o primeiro',
+		            'arcade vendido comercialmente em grande escala, antecipando o lançamento do Magnavox Odyssey por seis meses e Pong da Atari por um ano.'
+		        ].join(''),
+		        'full_text': [
+		            'Em 1971  os Estados Unidos da América começavam a retirar suas tropas do Vietnã e a geração paz e amor aos poucos dava espaço para o pessoal da discoteca.',
+		            'Em meio a este cenário de transição do protesto para a era da tecnologia e eletrônica, Nolan K. Bushnell, o então engenheiro-chefe da  Nutting Associates em parceiria com Ted Dabney , lança o Arcade Computer Space.',
+		            'Computer Space tem clara inspiração no game SpaceWar e foi o primeiro arcade produzido em grande escala.',
+		            'O game consiste em eliminar adversários que se assemelham a um disco voador; para isso você comanda uma espaçonave parecida com um ônibus espacial e que poderia ser tripulado pelos Thunderbirds.',
+		            'Bushnell transformou o quarto da sua filha em uma oficina para poder criar uma máquina que devido ao espaço que os computadores da época ocupavam, serviria apenas para jogar o SpaceWar!, e essa máquina foi chamada de Computer Space. O Computer Space foi o primeiro arcade do mundo e também o primeiro jogo eletrônico a ser comercializado no mundo. Como a experiência deu certo, a Nutting Associates contrata Bushnell temporariamente para montar os arcades Computer Space. Mais tarde Bushnell sai da empresa e funda a Atari.'
+		        ].join(''),
+		        'thumbnail': 'thumbnail_example.png',
+		        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+		        'images': [
+		            {'name': 'picture1.png', 'caption': "description1"},
+		            {'name': 'picture2.png', 'caption': "description2"},
+		            {'name': 'picture3.png', 'caption': "description3"}
+		        ],
+		        'video_gameplay': [
+		            {'url': 'http://www.youtu.be/Nhdu2Jh9cuc', 'caption': "Computer Space gameplay", 'type': "youtube"} // type: youtube or vimeo
+		        ],
+		        'demos': [],
+		        'references': [
+		            {'url': 'http://www.technologizer.com/2011/12/11/computer-space-and-the-dawn-of-the-arcade-video-game/', 'caption': "Computer Space and the Dawn of the Arcade Video Game"},
+		            {'url': 'http://blablagames.net/?p=910', 'caption': "BLA…Bla…Games"},
+		            {'url': 'http://en.wikipedia.org/wiki/Computer_Space', 'caption': "Computer Space"},
+		            {'url': 'https://gamesample.wordpress.com/tag/computer-space/', 'caption': "A historia dos jogos eletrônicos parte 2 – O nascimento dos Arcades e consoles"}
+		        ]
+			},
+			{
 			    'type': 'game', // game or console
 			    'name': 'Pong',
 			    'authors': ['Allan Alcorn'],

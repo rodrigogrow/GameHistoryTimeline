b44749a0003a132bfaeebeae3284ed45f3b6a22e
Rodrigo Braga <rodrigogrow@gmail.com>
2015-03-25 14:54:42 -0400

    update OxO

--- content/games_1950.js
+++ content/games_1950.js
@@ -63,37 +63,63 @@
 		        ]
 			},
 			{
+				'type': 'game',
 		        'name': 'OxO - Naughts and Crosses',
-		        'authors': ['S. Douglas', 'Alexander'],
+		        'authors': ['Alexander Shafto Douglas'],
 		        'year': '1952',
-		        'headline': 'OxO - O primeiro video game.',
+		        'headline': 'OxO - O primeiro jogo digital.',
 		        'resume': [
 		            'Foi em 1952 que surgiu o primeiro vídeo game, do jeito que deve ser, ',
 		            'gráficos digitais exibidos em um monitor de tubo de raios catódicos., ',
-		            'OXO, Noughts and Crosses foi desenvolvido por Alexander S. Douglas, ',
+		            'OXO, Noughts and Crosses foi desenvolvido por Alexander Shafto Douglas, ',
 		            'como parte da sua tese de doutorado Interação Homem-Computador, ',
 		            'O jogo tinha como controles um telefone analógico, onde cada número',
-		            'discado representava o quadro onde seria feito o movimento.'
+		            'discado representava o quadro onde seria feito o movimento. ',
+		            'Apesar da simplicidade e da aparente falta de emoção',
+		            'o jogo chamava atenção por representar bem a “Interação Homem-Computador”. ',
+		            'Apesar de brilhante, OXO não se popularizou porque só funcionava ',
+		            'no computador em que foi desenvolvido – o EDSAC – e ele era o único no mundo!',
+		            'A.S Douglas ganhou seu título de doutor e OXO foi considerado o primeiro vídeo game'
 		        ].join(''),
 		        'full_text': [
-		            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-		            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
-		            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-		            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-		            'etiam tempus wisi semper montes odio, lorem vitae vel eu ',
-		            'convallis. Volutpat blandit egestas et libero eu per, venenatis ',
-		            'aenean, proin quis aliquam viverra potenti vestibulum auctor, ',
-		            'non amet tellus sodales. In ullamco tellus risus nisl morbi amet. ',
-		            'Vel quisque nunc in placerat. Vitae id urna, nulla suspendisse, ',
-		            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
-		            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+		            'Somente em 1952 foi que surgiu o primeiro vídeo game, do jeito que deve ser - gráficos digitais',
+		            'exibidos em um monitor de tubo de raios catódicos. OXO, Noughts and Crosses foi desenvolvido',
+		            'por Alexander Shafto Douglas como parte da sua tese de doutorado “Interação Homem-Computador”. ',
+		            'OXO, também conhecido como "Noughts and Crosses" (Zeros e Cruzes), ',
+		            'é um jogo da velha de computador feito para o computador EDSAC em 1952.',
+		            'Já dentro dos quartéis, alguns militares aproveitavam o tédio para se divertir ',
+		            ' e fazer alguma coisa nos computadores que só eles tinham. Surgiram alguns jogos, ',
+		            'é verdade, mas eles eram analógicos e não podiam ser caracterizados como vídeo games ',
+		            'porque ou não tinham gráficos, ou os que tinham não eram exibidos em um monitor, ',
+		            'mas em um display vetorial.',
+		            'O jogo na realidade trata-se nada mais nada menos do que o famoso ',
+		            '“Jogo da Velha”, só que jogado em um computador com um controle de telefone analógico.',
+		            'O esquema da jogatina é o mesmo desde sempre: posicione o “X” ou a “O” no lugar que ',
+		            'você quer e feche uma linha com eles. Apesar da simplicidade e da aparente falta de ',
+		            'emoção, o jogo chamava atenção justamente por representar muitíssimo bem a “Interação ',
+		            'Homem-Computador”, coisa que pouca gente conhecia fora das universidades e recantos ',
+		            'militares.',
+		            'Apesar de brilhante, OXO não se popularizou porque só funcionava no computador  em  que ',
+		            'foi desenvolvido – o EDSAC – e ele era o único no mundo! Mesmo não tendo ganhado nem  ',
+		            'um centavo com o jogo, Alexander Shafto Douglas ganhou seu título de doutor e OXO foi considerado o ',
+		            'primeiro vídeo game do mundo!'
 		        ].join(''),
-		        'thumbnail': '',
-		        'main_img': '',
-		        'images': [],
-		        'video_gameplay': [],
-		        'demos': [],
-		        'references': []
+		        'thumbnail': 'oxo.jpg',
+		        'main_img': {'name': '1952-OXO.png', 'caption': "Emulaçao do OXO"},
+		        'images': [
+		            {'name': '1952-OXO.png', 'caption': "Emulador do jogo"},
+		            {'name': 'oxo.jpg', 'caption': "Digitalizaçao do OXO"},
+		            {'name': 'edsac.jpg', 'caption': "Computador utilizado"}
+		        ],
+		        'video_gameplay': [
+		            {'url': 'https://www.youtube.com/watch?v=Pkiwn7u8i8s', 'caption': "Gameplay do jogo OXO", 'type': "youtube"} // type: youtube or vimeo
+		        ],
+		        'references': [
+		            {'url': 'http://en.wikipedia.org/wiki/OXO', 'caption': "Game history"},
+		            {'url': 'http://contembits.com.br/historia/jogos/1952-OXO.aspx', 'caption': "História dos jogos eletrônicos"},
+		            {'url': 'http://www.nintendoblast.com.br/2010/01/historia-dos-video-games-1-o-comeco-de.html', 'caption': "O começo dos vídeos games"},
+		            {'url': 'http://www.muitointeressante.com.br/tags/oxo', 'caption': "História do oxo"}
+		        ]
 			},
 			{
 		        'type': 'game', // game or console

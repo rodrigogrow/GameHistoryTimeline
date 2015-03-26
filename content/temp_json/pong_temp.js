62989fa0a59a03a2b2f7da762a1ca664065ce716
TSantosFigueira <thiago.santos90@outlook.com>
2015-03-25 22:56:19 -0400

    Update pong_temp.js
    Editado Date, Full Text [adaptado e redigido pessoalmente], Resume, Headline, Video Gameplay (agora sem narração), além da Demo.

--- content/temp_json/pong_temp.js
+++ content/temp_json/pong_temp.js
@@ -4,25 +4,43 @@
     'authors': ['Allan Alcorn'],
     'company': ['Atari'],
     'year': '1972',
-    'date': '01-11-1972', // Day, Month, Year
-    'headline': 'Primeiro videogame lucrativo da História',
+    'date': '29-11-1972', // Day, Month, Year
+    'headline': 'The Newest 2 Player Video Skill Game',
     'resume': [
-        'Um dos primeiros videogames chamava-se Pong e representava um jogo',
+        'Um dos primeiros videogames chama-se Pong e representava um jogo',
         'de tênis para dois jogadores em uma televisão preto e branco. O objetivo ',
-        'era derrotar os adversários obtendo uma pontuação superior a eles.',
+        'é derrotar o adversário obtendo uma pontuação superior à dele.',
+        'Ainda hoje é possível encontá-lo em diversos sites destinados a jogos como',
+        'um de seus destaques.',
     ].join(''),
     'full_text': [
-        'Lançado em 1972 pela Atari, este foi sem dúvida o jogo que mais contribuiu',
-        'para a explosão da indústria dos videogames e foi também o primeiro',
-        'jogo lucrativo da história dos games. A sua criação deve-se a Allan',
-        'Alcorn. Allan desenvolveu características inovadoras para o Pong,',
-        'tais como a variação do ângulo da bola dependendo da parte como esta',
-        'era devolvida; O aumento gradual da velocidade do jogo e as variações',
-        'de sons. Para construir o protótipo e como necessitava de espaço para',
-        'os componentes, Alcorn comprou uma televisão Hitachi preto e branco ',
-        'e construiu uma cabine de madeira com um 1,20 mt de altura onde inseriu',
-        'a televisão os restantes circuitos integrados necessários ao ',
-        'funcionamento do jogo.'
+        'Lançado em 1972 pela Atari, foi sem dúvida o jogo que mais contribuiu',
+        'para a explosão da indústria dos videogames, mas além disso foi o ',
+        'primeiro jogo lucrativo da história. O fundador da Atari, Nolan Bushnell',
+        'atribuiu a tarefa a Allan Alcorn, o criador do jogo, como um exercício de treino.',
+        'Segundo Bushnell, a ideia baseou-se no jogo de ping-pong eletrônico do primeiro',
+        'console de jogos, o Magnavox Odyssey. Allan, contudo, levou a tarefa a sério e', 
+        'desenvolveu características novas e interessantes para seu jogo, com maior',
+        'destaque estão: a variação do ângulo da bola dependendo da parte como esta era',
+        'rebatida pelo paddle; o aumento gradual da velocidade do jogo e as variações',
+        'dos sons. Para construir o protótipo, Alcorn comprou uma televisão Hitachi preto',
+        'e branco, entretanto como necessitava de espaço para os componentes, construiu',
+        'uma cabine de madeira com um 1,20 mt de altura onde inseriu',
+        'a televisão e os demais circuitos necessários ao funcionamento',
+        'do jogo. A fase pública de testes iniciou-se em Setembro de 1972 e o local',
+        'escolhido foi o bar de um amigo, o Andy Capp’s Tavern. O protótipo',
+        'foi posicionado junto a outras máquinas de entretenimento: uma jukebox, uma',
+        'máquina de flippers e uma máquina com o Computer Space. Logo na primeira',
+        'noite o jogo teve uma recepção muito positiva e o foi obtendo sucesso cada vez ',
+        'maior nas semanas seguintes. Com esta enorme adesão, Bushnell decidiu avançar',
+        'com o desenvolvimento, após conseguir financiamento anunciou o lançamento do Pong',
+        'em 29 de Novembro de 1972. Em 1973 a Atari já exportava as suas máquinas para outros países.',
+        'Após o sucesso do Pong surgiram de imediato inúmeros esforços de outos produtores para ',
+        'copiarem o jogo e a fórmula do seu sucesso. Para contrariar o aumento de concorrência a Atari ',
+        'teve o que se pode considerar uma ideia genial: lançou a versão caseira do Pong em 1975 ',
+        'através de diversas lojas da Sears, em época de Natal. Nascia então o Home Pong.',
+        'A versão doméstica do jogo teve aproximadamente quatro vezes mais receita que a das máquinas',
+        'Arcade já distribuidas. Somente no Natal de 1975, venderam-se mais de 150.000 unidades.',
     ].join(''),
     'thumbnail': 'http://ts3.mm.bing.net/th?id=HN.608005174475620362&pid=1.7',
     'main_img': {'name': 'Pong' , 'caption': "Pong for Atari"},
@@ -32,7 +50,7 @@
         {'name': 'picture3.png', 'caption': "description3"}
     ],
     'video_gameplay': [
-        {'url': 'https://www.youtube.com/watch?v=RKF-yoX3V4k', 'caption': "Jogabilidade do clássico Pong", 'type': "youtube"} // type: youtube or vimeo
+        {'url': 'https://www.youtube.com/watch?v=hl9uZF-vP0U', 'caption': "Jogabilidade do clássico Pong", 'type': "youtube"} // type: youtube or vimeo
     ],
     'demos': [
         {'url': 'https://atari.com/arcade#!/arcade/pong/play', 'caption': "Jogue agora!"}

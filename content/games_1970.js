e02136c845750a323ba1b2020e8288069be6db02
Vinícius Cerqueira Silva <vini.cerqueira12@gmail.com>
2015-03-26 20:47:15 -0400

    Update games_1970.js
    breakout

--- content/games_1970.js
+++ content/games_1970.js
@@ -245,32 +245,31 @@
 			    'date': '13-05-1976', // Day, Month, Year
 			    'headline': 'Break blocks!',
 			    'resume': [
-			        'Breakout é um jogo eletrônico para Arcade desenvolvido',
-			      	'pela Atari, Inc. Influenciado pelo pong, seu objetivo',
-			      	'é destruir todos os blocos da parte de cima',
-			      	'com uma bola, sem deixá-la cair, controlando uma barra na',
-				    'parte de baixo da tela. Uma curiosidade é que o protótipo',
+			        'Breakout é um jogo eletrônico para Arcade desenvolvido ',
+			      	'pela Atari, Inc. Influenciado pelo pong, seu objetivo ',
+			      	'é destruir todos os blocos da parte de cima ',
+			      	'com uma bola, sem deixá-la cair, controlando uma barra na ',
+				    'parte de baixo da tela. Uma curiosidade é que o protótipo ',
 			      	'foi feito por Steve Jobs e Woz, mais tarde, fundadores da apple.'
 			    ].join(''),
 			    'full_text': [
-			        'Breakout é um jogo eletrônico para Arcade desenvolvido pela Atari,',
-			        'Inc e lançado em 13 de maio de 1976. Foi idealizado por Nolan Bushnell',
-			        'e Steve Bristow e influenciado pelo jogo de arcade de 1972 Pong, também da Atari.',
-			        'O jogo foi convertido para os consoles e atualizado como Super Breakout.',
+			        'Breakout é um jogo eletrônico para Arcade desenvolvido pela Atari, ',
+			        'Inc e lançado em 13 de maio de 1976. Foi idealizado por Nolan Bushnell ',
+			        'e Steve Bristow e influenciado pelo jogo de arcade de 1972 Pong, também da Atari. ',
+			        'O jogo foi convertido para os consoles e atualizado como Super Breakout. ',
 			        'Além disso, Breakout foi a base e inspiração para livros, ',
-			        'jogos, e o computador Apple II. No jogo, uma camada de tijolos são alinhados no topo da tela.',
-			        'A bola passa pela tela, rebatendo nas paredes laterais e superiores da tela.',
-			        'Quando um tijolo é atingido, a bola rebate de volta e o tijolo é destruído.',
-			        'O jogador perde uma vida quando a bola toca a parte inferior da tela.',
-			        'Para prevenir que isso aconteça, o jogador move uma paleta para rebater a bola',
-			        'para cima, mantendo-a no jogo. O breakout foi baseado no Pong, mudando apenas a mecânica do jogo' 
+			        'jogos, e o computador Apple II. No jogo, uma camada de tijolos são alinhados no topo da tela. ',
+			        'A bola passa pela tela, rebatendo nas paredes laterais e superiores da tela. ',
+			        'Quando um tijolo é atingido, a bola rebate de volta e o tijolo é destruído. ',
+			        'O jogador perde uma vida quando a bola toca a parte inferior da tela. ',
+			        'Para prevenir que isso aconteça, o jogador move uma paleta para rebater a bola ',
+			        'para cima, mantendo-a no jogo. O breakout foi baseado no Pong, mudando apenas a mecânica do jogo ' 
 			        ].join(''),
-			    'thumbnail': 'thumbnail_example.png',
-			    'main_img': {'name': 'main_picture.png', 'caption': "description"},
+			    'main_img': {'name': 'main_picture.png', 'caption': "Flyer Breakout"},
 			    'images': [
-			        {'name': 'picture1.png', 'caption': "description1"},
-			        {'name': 'picture2.png', 'caption': "description2"},
-			        {'name': 'picture3.png', 'caption': "description3"}
+			        {'name': 'picture1.png', 'caption': "Gameplay do Breakout"},
+			        {'name': 'picture2.png', 'caption': "Arcade do Breakout"},
+			        {'name': 'picture3.png', 'caption': "Cartucho para Atari 2600"}
 			    ],
 			     'video_gameplay': [
 			        {'url': 'http://www.youtu.be/JRAPnuwnpRs', 'caption': "Gameplay do Breakout", 'type': "youtube"} // type: youtube or vimeo

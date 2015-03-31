2ad3f895bdcb092df5488190f4c274a6676d23ce
BrazilGabriel <gabriel.br1994@gmail.com>
2015-03-31 08:21:52 -0400

    Update megaman_temp.js

--- content/temp_json/megaman_temp.js
+++ content/temp_json/megaman_temp.js
@@ -1,1 +1,51 @@
+{
+        'type': 'game', // game or console
+        'name': 'Mega Man',
+        'authors': [],
+        'company': ['CAPCOM'],  
+        'year': '1987',
+        'date': '20-06-1951', // Day, Month, Year
+        'headline': 'Frogger',
+        'resume': [
+            'O título foi lançado pela Konami em 1981 para os arcades ',
+            'e um ano depois em 1982 foi lançado para o Atari 2600 ',
+            'com algumas pequenas alterações gráficas devido às limitações ',
+            'de cada plataforma.O objetivo do jogo era muito simples: ',
+            'o jogador controlava um sapo, movendo-se para os lados, frente e trás ',
+            'que precisa atravessar uma rua repleta de veículos e em seguida ',
+            'um riacho, passando por cima de troncos que boiavam. Se o sapo ',
+            'fosse atropelado por qualquer veículo ou caísse na água, Acabava o jogo.'.
+        ].join(''),
+        'full_text': [
+            'Frogger foi um jogo criado em 1981 em uma parceria entre as então jovens',
+            ' empresas de informática SEGA e Konami. Inicialmente fabricado para ',
+            'máquinas de arcade, logo foi fabricado em grande escala para o console  ',
+            ' mais popular da época, o Atari 2600, tornando-se também  um de seus jogos  ',
+            'mais populares. A mecânica do jogo era simples, onde o jogador controla um sapo ',
+            'O sapo podia ser movimentado para os lados, para frente e para trás. ',
+            ' A cada cinco anfíbios que o jogador conseguisse colocardo outro lado do rio,  ',
+            ', um novo nível era iniciado. A dificuldade de cada fase era medida pela ',
+            'velocidade dos objetos na avenida e no rio, e tudo ficava bastante ',
+            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
+            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'video_url', 'caption': "description", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'reference_url1', 'caption': "description"},
+            {'url': 'reference_url2', 'caption': "description"},
+            {'url': 'reference_url3', 'caption': "description"}
+        ]
+}
 

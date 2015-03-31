8acce8d822d394b5d48da37d8c50eb802a31cbb5
BrazilGabriel <gabriel.br1994@gmail.com>
2015-03-31 19:26:51 -0400

    Update megaman_temp.js

--- content/temp_json/megaman_temp.js
+++ content/temp_json/megaman_temp.js
@@ -1,33 +1,79 @@
 {
         'type': 'game', // game or console
         'name': 'Mega Man',
-        'authors': [],
+        'authors': ['Keiji Inafune'],
         'company': ['Capcom'],  
         'year': '1987',
         'date': '17-12-1987', // Day, Month, Year
         'headline': 'Dificuldade extrema',
         'resume': [
-            'O título foi lançado pela Konami em 1981 para os arcades ',
-            'e um ano depois em 1982 foi lançado para o Atari 2600 ',
-            'com algumas pequenas alterações gráficas devido às limitações ',
-            'de cada plataforma.O objetivo do jogo era muito simples: ',
-            'o jogador controlava um sapo, movendo-se para os lados, frente e trás ',
-            'que precisa atravessar uma rua repleta de veículos e em seguida ',
-            'um riacho, passando por cima de troncos que boiavam. Se o sapo ',
-            'fosse atropelado por qualquer veículo ou caísse na água, Acabava o jogo.'.
+            '<p>Mega Man (Rockman) é uma franquia criada por Keiji Inafune pela empresa Capcom.'
+            'A série é bem conhecida, tendo mais de cinquenta versões lançadas, o que a torna '
+            'a série mais produtiva da Capcom. A franquia teve início em 1987 com o primeiro '
+            'Mega Man, que foi lançado para o Nintendo Entertainment System, também conhecido '
+            'como Famicom. Este jogo e suas sequências compõe a série original de jogos, mas '
+            'foram criadas várias sub-série spinoffs que se passam no mesmo universo fictício.',
+            'O primeiro jogo Mega Man veio de cara trazendo a marca registrada da série: '
+            'dificuldade extrema. O jogo era tão absorvente que, mesmo a quantidade absurda de'
+            'continues, não fez os jogadores deixarem o robô de lado. Após frustrações iniciais '
+            'com o sistema de controle, dificuldade desonesta e quedas do Mega Man, quem joga '
+            'este game é recompensado com paciência e aprendizagem, principalmente através de '
+            'retrospectiva de padrões de inimigos.</p>',
+            '<p>Nem mesmo o pesadelo dos blocos que desapareciam afastou aqueles que se tornariam '
+            'fãs da saga, porque a recompensa por percorrer toda uma fase para derrotar um chefe '
+            'era gratificante: absorver a habilidade do inimigo, algo até então pouco visto nos jogos. '
+            'Gráficos e cores impecáveis, trilha sonora de primeira, tornou-se um clássico no momento '
+            'do lançamento.</p>'
         ].join(''),
         'full_text': [
-            'Frogger foi um jogo criado em 1981 em uma parceria entre as então jovens',
-            ' empresas de informática SEGA e Konami. Inicialmente fabricado para ',
-            'máquinas de arcade, logo foi fabricado em grande escala para o console  ',
-            ' mais popular da época, o Atari 2600, tornando-se também  um de seus jogos  ',
-            'mais populares. A mecânica do jogo era simples, onde o jogador controla um sapo ',
-            'O sapo podia ser movimentado para os lados, para frente e para trás. ',
-            ' A cada cinco anfíbios que o jogador conseguisse colocardo outro lado do rio,  ',
-            ', um novo nível era iniciado. A dificuldade de cada fase era medida pela ',
-            'velocidade dos objetos na avenida e no rio, e tudo ficava bastante ',
-            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
-            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+            '<p>Na série, dois gênios da robótica, Dr. Albert Wily e Dr. Thomas Light, criam'
+            'uma nova geração de robôs superior a quaisquer outros já criados (Robot Masters).'
+            'Então, Wily, ex-sócio de Dr. Light (criador de Mega Man), vendo o incrível'
+            'potencial dos robôs, começa a imaginar que, com eles sob seu comando, poderia'
+            'ter um enorme poder nas mãos. Ao compartilhar seus planos de domínio com Light'
+            'e ter retorno negativo, rouba seu projeto inicial, Protoman (Blues, no original),'
+            'para servi-lo, junto com outros robôs.</p>',
+            '<p>Dr. Light, então, cria Rock, um robô ajudante de laboratório e Roll, uma ajudante'
+            'doméstica. Quando Wily resolve usar os Robôs para cometer terrorismo, Rock não'
+            'entende por que seus irmãos estariam cometendo tais barbaridades e pede para'
+            'Dr. Thomas Light torná-lo num robô de batalha para deter Wily. Surge então Mega Man,'
+            'o robô azul que sempre estraga os planos malignos de Wily.</p>',
+            '<p>Mega Man é um jogo de plataforma clássico, side-scrolling, e um dos elementos que '
+            'chama atenção é sua jogabilidade não-linear, onde o jogador pode escolher começar '
+            'por qualquer um dos 6 estágios do jogo (a ordem que você escolhe não influencia na '
+            'história do jogo) e, ao fim de cada estágio, enfrenta um Robot Master (os chefões) e, '
+            'vencendo esse robô, ganha uma arma referente a ele que permite ao Mega usar a habilidade '
+            'do "falecido" (falecido entre aspas, porque robô não morre, vira ferro velho). Essa arma, '
+            'ao contrário do Mega Buster, tem munição limitada que pode, felizmente, ser recarregada '
+            'coletando os Weapon Energies, que são deixados por inimigos destruídos durante as fases '
+            'do game.</p>',
+            '<p>A barra de life do robozinho azul também pode ser recarregada coletando o item Life Energy.'
+            'Esse esquema de adquirir a arma do inimigo é muito legal, pois cada Robot Master possui '
+            'sua fraqueza, por exemplo: a arma que melhor funciona contra o Ice Man é o Elec Beam que '
+            'você obtém ao derrotar o Elec Man, já a fraqueza de Elec Man é a Cut Blade que você descola '
+            'ao destruir o Cut Man, e assim por diante.</p>
+            '<p>Algumas curiosidades:</p>'
+            '<p>Os nomes de Rock e Roll são mesmo uma homenagem ao estilo musical rock'n'roll.</p>'
+            '<p>Nos Estados Unidos, o vice-presidente da Capcom Joseph Marici mudou o nome de '
+            'Rockman para Mega Man, simplesmente, pelo fato de não ter gostado do nome original japonês.</p>'
+            '<p>O nome completo de Dr. Light é Thomas Light e é uma clara homenagem a Thomas Edison e o seu '
+            'visual foi inspirado no Papai Noel. Já Dr. Wily tem como nome completo Albert Wily, uma homenagem '
+            'a Albert Einstein, inclusive seu visual foi inspirado em Einstein. O vilão foi inspirado nele, '
+            'exatamente porque Einstein tem muito a ver com a bomba atômica (que fez estragos no Japão)</p>.'
+            '<p>O visual e design do game tem muitas influências de anime e mangá, o robozinho tem o visual '
+            'fortemente influenciado pelo visual do Astro Boy. Dizem que a Capcom possuía uma licença para '
+            'lançar um jogo do Astro Boy, mas a licença expirou e a produtora resolveu utilizar o que já tinha '
+            'feito nesse jogo no game do Mega Man.</p>'
+            '<p>Originalmente, Mega Man seria vermelho, mas devido a limitação de 54 cores da paleta do Famicom, '
+            'resolveram fazê-lo azul, que era uma cor com várias tonalidades na paleta do videogame.</p>'
+            '<p>Mega Man não vendeu muito bem e Inafune considera a arte da capa americana do game como um dos '
+            'motivos para vendas tão baixas no ocidente. E, por sinal, a capa é bem bizarra: no lugar de um garoto, '
+            'um cara meio tiozinho com armadura amarela e azul segurando uma pistola e sem o Mega Buster. Com o '
+            'passar do tempo, a capa foi se tornando motivo de piada. A capa ganhou o título de capa mais feia da '
+            'história dos games pelo site GameSpy.</p>'
+            '<p>Mega Man é o único jogo da série a possuir um sistema de pontuação, e é algo totalmente inútil, '
+            'porque não há nenhuma recompensa por pontos obtidos e o cartucho não era capaz de salvar seus recordes.</p>'
+
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},
@@ -37,7 +83,7 @@
             {'name': 'picture3.png', 'caption': "description3"}
         ],
         'video_gameplay': [
-            {'url': 'video_url', 'caption': "description", 'type': "youtube"} // type: youtube or vimeo
+            {'url': 'https://www.youtube.com/watch?v=slD8IzS5kOA', 'caption': "Gameplay Mega Man - NESguide.com", 'type': "youtube"} // type: youtube or vimeo
         ],
         'demos': [
             {'url': 'demonstration_url', 'caption': "description"}

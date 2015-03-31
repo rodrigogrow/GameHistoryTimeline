71d6772ea03ac8ec65e370fc95a0eb4a362229e1
TeusMa <ma.santos.s@hotmail.com>
2015-03-31 20:18:08 -0300

    Update games_1980.js

--- content/games_1980.js
+++ content/games_1980.js
@@ -62,7 +62,76 @@
 		            {'url': 'http://www.computerhistory.org/revolution/computer-games/16/189/2213', 'caption': "Detalhes sobre space war"},
 		            {'url': 'http://pdp-1.computerhistory.org/pdp-1/index.php?f=theme&s=4&ss=3', 'caption': "Mais sobre o projeto space war"}
 		        ]
-			}
+			},
+			{
+        'type': 'game', // game or console
+        'name': 'Street Fighter',
+        'authors': ['Hiroshi Matsumoto e Takashi Nishiyama'],
+        'company': ['Capcom CO., Ltd.'],  
+        'year': '1983',
+        'date': '11 de Junho de 1983', // Day, Month, Year
+        'headline': 'O primeiro game da série, que é referência em jogos de luta',
+        'resume': [
+            'O primeiro game da série, que é referência em jogos de luta, , ',
+            'foi lançado em 30 de agosto de 1987, lançado pela Capcom nos arcades japoneses, ',
+            'O jogo, porém, começou de maneira um tanto restrita: o jogador não podia escolher um personagem, , ',
+            'e contava apenas com Ryu , representando o Japão. Se um segundo lutador entrasse na partida, ',
+            'ele automaticamente assumiria o comando de Ken, representando os Estados Unidos. '
+        ].join(''),
+        'full_text': [
+            'Uma das características interessantes do primeiro Street Fighter, ',
+            'é que seu gabinete teve duas diferentes versões: a primeira delas apresentava apenas dois botões, ',
+            'um para soco, outro para chute – e os golpes fracos, médios ou fortes, ',
+            'eram desferidos de acordo com a força com que o jogador os pressionava., ',
+            'Como estes botões eram sensíveis à pressão, acabavam estragando. Posteriormente ',
+            'as máquinas foram substituídas pelas versões com seis botões normais que perduram até hoje. ',
+            'A indústria era bem diferente em 1987. Nesta época já havia games de luta, ',
+            ', mas a maioria era de progressão lateral cooperativa, como no clássico Double Dragon. ',
+            'Competir no fliperama era basicamente sinônimo de escolher um game  ',
+            'e batalhar pela pontuação mais alta, com os jogadores se alternando no controle.  ',
+            'A disputa mano a mano entre dois jogadores, grande inovação do jogo,'
+            'colocava o segundo player no comando do americano Ken idêntico a Ryu, mas com outra paleta de cores.  ,'
+            'A novidade causou curiosidade, mas esbarrou em um defeito terrível para qualquer jogo de luta: , '
+            'a jogabilidade. Os comandos eram bastante complexos se comparados aos títulos  '
+            'de luta da época. Para piorar, os personagens andavam saltitando e os controles respondiam mal, ,  '
+            'o que comprometeu o sistema de execução de golpes baseado na combinação de movimentos com , '
+            'o joystick + algum botão de ataque: executar um simples hadouken era mais uma questão de persistência do que habilidade.'
+            'Fechando o pacote deste clássico, temos alguns bônus interessante: ' 
+            'Nesse jogo, existe uma fase bônus que consiste em quebrar uma pilha de pedras com um golpe. '
+            'Desconfiam que foi daí que a série Mortal Kombat tirou seus desafios do tipo “Test Your Might'
+            'O primeiro jogo foi esquecido por conta do sucesso de Street Fighter II '
+            '(para alguns, considerado o primeiro jogo da franquia)'
+            'Foi o primeiro projeto em que Keiji Inafune (co-criador de Megaman) trabalhou. '
+            'O diretor e produtor do Street Fighter original, Takashi Nishiyama, juntamente com o planejador'
+            'Hiroshi Matsumoto, deixaram a Capcom após eles criaram SF1 e se juntou SNK onde eles projetaram Fatal Fury e Art of Fighting ' 
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'img1.png', 'caption': "Arte do jogo "},
+            {'name': 'img2.png', 'caption': " 1 maquina '"},
+            {'name': 'img3.png', 'caption': "Maquina atualizada "}
+            {'name': 'img4.png', 'caption': "Bonus Stage "}
+            {'name': 'img5.png', 'caption': "Personagens do jogo"}
+        ],
+        'video_gameplay': [
+            {'url': 'Gameplay: https://www.youtube.com/watch?v=181bEWh0KqI', 'caption': "Gameplay do Jogo", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'http://pt.slideshare.net/jpagini/street-fighter-46475822', 'caption': "Introdução aos Jogos Eletrônicos (UEA) - Street Fighter", 'type': 'slides'},
+            {'url': 'http://www.arkade.com.br/street-fighter-completa-24-anos-relembre-primeiro-game-serie/
+', 'caption': "STREET FIGHTER COMPLETA 24 ANOS! RELEMBRE O PRIMEIRO GAME DA SÉRIE!"},
+            {'url': 'http://www.kotaku.com.br/street-fighter-onde-tudo-comecou/', 'caption': "Street Fighter: onde tudo começou"},
+            {'url': 'http://pt.wikipedia.org/wiki/Street_Fighter', 'caption': "Street Fighter"}
+            {'url': 'http://pt.wikipedia.org/wiki/Capcom', 'caption': "Capcom"}
+            {'url': 'http://www.fightersgeneration.com/games/sf.html', 'caption': "Street Fighter"}
+            {'url': 'http://www.techtudo.com.br/noticias/noticia/2012/03/especial-a-historia-da-serie-street-fighter-parte-1.html
+', 'caption': "Especial: a história da série Street Fighter – Parte 1"}
+        ]
+}
 		]
 	}
 

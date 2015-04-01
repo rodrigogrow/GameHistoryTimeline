50a8d7b2946725d561e89a96a307619025d2a3d6
marcosdacio <marcosdacio@hotmail.com>
2015-04-01 01:25:56 -0300

    Update Tetris_temp.js

--- content/temp_json/Tetris_temp.js
+++ content/temp_json/Tetris_temp.js
@@ -12,19 +12,37 @@
             'Pavlovsky eram engenheiros informáticos no Centro de Computadores da Academia Russa das Ciências e Vadim era um aluno com 16 anos. ',
             'Tetris foi um dos primeiros itens de exportação de sucesso da União Soviética',
             'e um dos primeiros a ser visto como um tipo de vício.Atingiu um público alvo inédito na história dos videogames. '
+            'Alexey Pajitnov conheceu o quebra-cabeças Pentaminó e decidiu criar uma versão virtual dele para seu computador Electronica 60.',
+             ' Removeu um dos blocos do jogo e nomeou com o prefixo quatro em grego: Tetris ',
+             'O jogo consiste em empilhar tetraminós que descem a tela de forma que completem linhas horizontais.'
+             'Quando uma linha se forma, ela se desintegra, as camadas superiores descem, e o jogador ganha pontos.'
+             'Quando a pilha de peças chega ao topo da tela, a partida se encerra.'
+             'Pajitnov percebeu o potencial do jogo por não conseguir parar de jogar antes mesmo de terminar o programa,'
+            'bem como dois colegas de trabalho que ajudaram a finalizar o jogo e todos os colegas do centro de computação, '
+            'que haviam recebido cópias em disquetes gravados pelo próprio Pajitnov. Para não ser acusado de viciar os pesquisadores '
+            'num passatempo eletrônico, destruiu todos os discos ao fim do expediente.Entretanto, o jogo continuou sendo distribuído, '
+            'de maneira informal, até chegar a pessoas interessadas em comercializá-lo.'
         ].join(''),
-        'full_text': [
-            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
-            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu ',
-            'convallis. Volutpat blandit egestas et libero eu per, venenatis ',
-            'aenean, proin quis aliquam viverra potenti vestibulum auctor, ',
-            'non amet tellus sodales. In ullamco tellus risus nisl morbi amet. ',
-            'Vel quisque nunc in placerat. Vitae id urna, nulla suspendisse, ',
-            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
-            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+        'full_text': 
+            'Quando Alexey Pajitnov desenvolveu a primeira versão de Tetris, disponibilizando a'
+            'versão final no dia 6 de junho de 1984, ele não imaginava que um projeto menor que '
+            'criou enquanto trabalhava para o Centro de Computação da Academia de Ciências Soviética '
+            'tornar-se-ia uma verdadeira coqueluche. Baseado em estudos de jogos matemáticos e poliominós iniciados nos anos 50 pelo professor Solomon W. Golomb, '
+            'ele deu ao game o nome de Tetris, baseado na palavra grega Tetra (quatro) e no seu '
+            'esporte favorito, o tênis. Os pentaminós são jogos de mesa onde você encaixa peças '
+            'geométricas compostas por cinco quadrados em um quadrado ou um retângulo. Para o game, Alexey utilizou peças de quatro quadrados.'
+            'O game foi desenvolvido originalmente para o computador soviético Elektronika 60'
+            'em conjunto com Dmitri Pavlovsky e Vadim Gerasimov, este último responsável por '
+            'portá-lo para o IBM PC, o que foi crucial para que Tetris se tornasse popular. Em '
+            'pouco tempo o jogo se alastrou por Moscou, não demorando muito para atingir '
+            'estados-satélites do regime soviético. Foi na Hungria, mais precisamente em '
+            'Budapeste que desenvolvedores locais começaram a converter Tetris para outras '
+            'plataformas. Foi lá também que a confusão dos direitos sobre o game começou.'
+            ''
+            ''
+            ''
+            ''
+            ''
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},
@@ -41,7 +59,7 @@
             {'url': 'https://www.youtube.com/watch?v=NhwNTo_Yr3k', 'caption': "Jogue Tetris"}
         ],
         'references': [
-            {'url': 'reference_url1', 'caption': "description"},
+            {'url': 'http://meiobit.com/289280/tetris-30-anos-uma-historia-de-roubos-trapacas-e-muito-mais/', 'caption': "Tetris 30 anos"},
             {'url': 'reference_url2', 'caption': "description"},
             {'url': 'reference_url3', 'caption': "description"}
         ]

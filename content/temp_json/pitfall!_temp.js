800210c2b58edac2d71d066b044291e3c4b3d8f4
lukebrag <l2klucas@gmail.com>
2015-04-01 04:36:23 -0300

    Create pitfall!_temp.js

---
+++ content/temp_json/pitfall!_temp.js
@@ -0,0 +1,56 @@
+{
+        'type': 'game', // game or console
+        'name': 'Pitfall!',
+        'authors': ['David Crane'],
+        'company': ['Activision'],  
+        'year': '1982',
+        'date': 'Setembro de 1982', // Day, Month, Year
+        'headline': 'O segundo jogo mais vendido para Atari 2600',
+        'resume': [
+            'Pitfall! é um jogo eletrônico lançado pela Activision ',
+            'em 1982. É um dos jogos mais vendidos para o Atari 2600, ',
+            'com mais de 4 milhões de cópias vendidas. O jogador deve ',
+            'mover seu personagem, conhecido como Pitfall Harry, ',
+            'através de uma floresta tipo labirinto numa tentativa ',
+            'de recuperar 32 tesouros em um período de 20 minutos. ',
+            'Nesse tempo ele deve derrotar a densa floresta, com suas ',
+            'cobras, morcegos e insetos.'
+        ].join(''),
+        'full_text': [
+            'No caminho Pitfall deve superar inúmeros riscos, incluindo ',
+            'poços de piche, areia movediça, buracos, troncos de árvore ',
+            'parados ou rolando, cascavel, escorpião, fogo, morcego, e ',
+            'crocodilo. Harry pode pular sobre esses obstáculos, ou ',
+            'evitá-los de outras maneiras como escalar, correr e se ',
+            'abaixar na hora certa, e, em certos lugares, se balançar ',
+            'sob cipós.',
+            'Pitfall! foi criado por David Crane, um programador que ',
+            'trabalhou para a Activision no início da década de 1980. ',
+            'Em uma entrevista para a revista Edge em novembro de 2003, ',
+            'David descreveu como, em 1979, ele desenvolveu a tecnologia ',
+            'para mostrar uma imagem mais realística de um homem ',
+            'correndo, e em 1982, como ele estava a procura de um jogo ',
+            'adequado para usá-lo.',
+            'O jogo alcançou detalhes técnicos inéditos, como uma ausência ',
+            'de cintilação, multi-colorido e sprites, em um sistema com um ',
+            'hardware reconhecidamente primitivo.'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=MhXMYw1lXY0', 'caption': "Gameplay", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'http://www.jogosdeatari.com.br/game/pitfall', 'caption': "Demo"}
+        ],
+        'references': [
+            {'url': 'http://en.wikipedia.org/wiki/Pitfall!', 'caption': "Wikipedia"},
+            {'url': 'http://www.atariage.com/software_page.html?SoftwareLabelID=360', 'caption': "AtariAge"},
+            {'url': 'reference_url3', 'caption': "description"}
+        ]
+}

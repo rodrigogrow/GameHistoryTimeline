59e2ac040ea272934181eeb6d43e44031ca95046
Gabriel014 <formmo@live.com>
2015-04-04 12:11:41 -0400

    Update games_1980.js

--- content/games_1980.js
+++ content/games_1980.js
@@ -132,6 +132,61 @@
 ', 'caption': "Especial: a história da série Street Fighter – Parte 1"}
         ]
 }
+	{
+        'type': 'game', // game or console
+        'name': 'Super Mario Bros',
+        'authors': [ 'Shigeru Miyamoto'],
+        'company': ['Nintendo'],  
+        'year': '1985',
+        'date': '13-09-1985', // Day, Month, Year
+        'headline': 'O jogo que salvou a indústria',
+        'resume': [
+            'Criado por Shigeru Miyamoto, Super Mario Bros é o',
+            'primeiro jogo "oficial" do encanador conhecido em todo',
+            'o mundo. Derivado do primeiro jogo de Mario, que por sua vez',
+            'originou-se do jogo de Donkey Kong, esse jogo tinha uma',
+            'mecânica diferente e inovadora, revolucionando a indústria dos',
+            'videogames e até salvando-a.',
+        ].join(''),
+        'full_text': [
+            'O jogo é o mais vendido de toda a história dos videogames (contando-se aí',
+            'os jogos vendidos junto com os consoles) com mais de 40 milhões de cópias',
+            'e foi o principal responsável pelo sucesso inicial do console NES.',
+            'Foi o primeiro sucesso do designer de jogos japonês Shigeru Miyamoto.',
+            'Em 1993, foi lançado para a plataforma Super Nintendo, dentro da coletânea',
+            'Super Mario All-Stars, com gráficos melhorados e possibilidade de salvamento',
+            'do jogo durante seu progresso. O jogador controla o principal protagonista da',
+            'série, Mario. O objetivo do jogo é percorrer o Reino do Cogumelo (Mushroom Kingdom),',
+            'sobreviver às forças do principal vilão, Bowser, e salvar a Princesa Peach.',
+            'Super Mario Bros. é dividido em oito mundos, e cada um dos quais possui quatro fases.',
+            'De acordo com o manual do jogo, a história se baseia no próprio Reino dos Cogumelos:',
+            '"Um dia, o pacífico Reino dos Cogumelos foi invadido pelos Koopas, uma tribo de tartarugas',
+            'famosas pelo uso de magia negra. Os pacíficos e calmos habitantes do reino foram transformados',
+            'em pedra, barro e mesmo em flores pelo caminho. Assim, o Reino entrou em ruína.',
+            'A única pessoa que pelo menos tenta enfrentá-los é Princesa Peach, filha do rei do local.',
+            'Mas infelizmente, ela está sendo mantida em cativeiro pelo Rei dos Koopas: Bowser que quer se',
+            'casar com ela para poder virar rei do seu reino. Sabendo disso, um encanador chamado Mario decide',
+            'que irá salvar a princesa e assim libertar todos os habitantes do reino…"',
+            ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "Capa Super Mario Bros"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "Gameplay do Super Mario Bros"},
+            {'name': 'picture2.png', 'caption': "Cartucho do Super Mario Bros"},
+            {'name': 'picture3.png', 'caption': "Castelo do chefe do World 1"}
+        ],
+         'video_gameplay': [
+            {'url': 'https://www.youtube.com/watch?v=ia8bhFoqkVE', 'caption': "Super Mario Bros. - NES Gameplay", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'http://www.letsplaysnes.com/play-super-mario-bros-online-nes/', 'caption': "Um emulador de Super Mario Bros online"}
+        ],
+        'references': [
+            {'url': 'http://pt.wikipedia.org/wiki/Super_Mario_Bros.', 'caption': "Wikipédia"}
+            {'url': 'http://pt.slideshare.net/gabrielalonso927/breakout-1976', 'caption': "Slides"}
+            {'url': 'https://www.youtube.com/watch?v=ZH2wGpEZVgE', 'caption': "Como foi feito o Level Design do jogo"}
+        ]
+}
 		]
 	}
 

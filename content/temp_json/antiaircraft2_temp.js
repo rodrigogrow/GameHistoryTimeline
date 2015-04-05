f15b0b803c6200a31251fbe3c52e58d739ac2257
RickoBraz <marco.henrique.s.s@gmail.com>
2015-04-05 01:01:26 -0300

    Update antiaircraft2_temp.js

--- content/temp_json/antiaircraft2_temp.js
+++ content/temp_json/antiaircraft2_temp.js
@@ -46,6 +46,6 @@
             {'url': 'http://www.armchairarcade.com/neo/node/577&prev=search', 'caption': "ArmChair Arcade"},
             {'url': 'http://en.wikipedia.org/wiki/Anti-Aircraft_(video_game)&prev=search', 'caption': "Wikipédia"},
             {'url': 'http://www.arcade-museum.com/game_detail.php%3Fgame_id%3D6892&prev=search', 'caption': "Arcade Museum"},
-            {'url': 'http://pt.slideshare.net/RickoBraz/antiaircraft2', 'caption': "Slides"}
+            {'url': 'http://pt.slideshare.net/RickoBraz/antiaircraft2', 'caption': "Slides" }
         ]
 }

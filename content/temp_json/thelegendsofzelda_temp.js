ae7b418358556c02c1c4803db016cb8caab1f134
RickoBraz <marco.henrique.s.s@gmail.com>
2015-04-05 01:02:22 -0300

    Create thelegendsofzelda_temp.js

---
+++ content/temp_json/thelegendsofzelda_temp.js
@@ -0,0 +1,51 @@
+{
+        'type': 'Arcade', // game or console
+        'name': 'Anti-Aircraft 2',
+        'authors': [],
+        'company': ['Atari'],  
+        'year': '1975',
+        'date': '06-06-1975', // Day, Month, Year
+        'headline': 'O primeiro jogo a possuir um MOD',
+        'resume': [
+            'Anti-Aircraft 2 foi um jogo de um ou dois jogadores, ',
+            'onde Aviões passam de um lado para o outro ',
+            'enquanto o jogador controla duas baterias antiaéreas,',
+            'localizadas no lado inferior esquerdo e direito da tela, ',
+            'disparando mísseis contra esses aviões. ',
+            'Aquele que derrubasse mais aviões ganhava.'
+        ].join(''),
+        'full_text': [
+            'Anti-Aircraft 2 foi um jogo de um ou dois jogadores, ',
+            'onde Aviões passam de um lado para o outro ',
+            'enquanto o jogador controla duas baterias antiaéreas,',
+            'localizadas no lado inferior esquerdo e direito da tela, ',
+            'disparando mísseis contra esses aviões. ',
+            'Aquele que derrubasse mais aviões ganhava.',
+            'Os controles do jogo eram basicamente ',
+            'para cima e para baixo e o botão de atirar, ',
+            'sendo que os botões “para cima” e “para baixo” ',
+            'moviam a mira da arma para uma das três posições predefinidas. ',
+            'O jogo está alojado em um gabinete personalizado ',
+            'incluindo dois conjuntos (um conjunto por jogador) ',
+            'de três botões de controle (para cima, para baixo, e fogo).',
+            'Existiu posteriormente, uma opção não documentada ', 
+            'que disponibilizava a mudança de aviões para OVNI\'s .'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "Tela Frontal do Game"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "Maquina"},
+            {'name': 'picture2.png', 'caption': "Tela Frontal do Game"}
+        ],
+        'video_gameplay': [
+            {'url': 'http://www.youtube.com/watch?v=bOyH_D_XLWU','caption': "Versão Original", 'type': "youtube"},
+            {'url': 'http://www.youtube.com/watch?v=GgH7-aFQWQo','caption': "Versão Modificada", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [],
+        'references': [
+            {'url': 'http://www.armchairarcade.com/neo/node/577&prev=search', 'caption': "ArmChair Arcade"},
+            {'url': 'http://en.wikipedia.org/wiki/Anti-Aircraft_(video_game)&prev=search', 'caption': "Wikipédia"},
+            {'url': 'http://www.arcade-museum.com/game_detail.php%3Fgame_id%3D6892&prev=search', 'caption': "Arcade Museum"},
+            {'url': 'http://pt.slideshare.net/RickoBraz/antiaircraft2', 'caption': "Slides"}
+        ]
+}

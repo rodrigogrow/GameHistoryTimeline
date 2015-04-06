b29979b7089fa841c900a1b6f3dc6adf0ceb8efd
mattizinho <matt.vieira12@hotmail.com>
2015-04-05 22:18:48 -0300

    Update Bully_temp.js

--- content/temp_json/Bully_temp.js
+++ content/temp_json/Bully_temp.js
@@ -7,15 +7,27 @@
         'date': '17-10-2006', // Day, Month, Year
         'headline': 'Bully',
         'resume': [
-            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
-            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu '
+            'Bully é um jogo eletrônico de ação-aventura em terceira pessoa, lançado ',
+            'pela Rockstar Games. O jogador entrará num mundo de intrigas, fofocas, ',
+            'lutas e entraves, que todo colégio tem. "Bully" conseguiu capturar a ',
+            'essência do colegial em seu todo, não importa país, cultura ou língua. ',
+            'A história se passa com o problemático James Hopkins "Jimmy" na escola ',
+            'integral Bullworth Academy, a escola mais louca de todo o país, única que ',
+            'aceitou o aluno. Jimmy tem uma longa ficha suja de escolas passadas, como ',
+            'Vandalismo, Linguagem imprópria, Graffiti, e Bullying. Como o jogo se passa '
+            'em um internato, você terá de participar das aulas, que dão algum prêmio, ',
+            'como as aulas de inglês, que dão à Jimmy um vocabulário maior para pedir ',
+            'desculpas aos monitores e colegas que querem agredi-lo. Aulas de química ',
+            'ensinam a fazer bombinhas, bombas fedorentas, pó de mico, etc.; ',
+            'Bully é o típico jogo da Rockstar com violência e muita diversão, deixando ',
+            'o jogador livre para escolher andar pela cidade e fazer bagunça, ou seguir ',
+            'seu objetivo específico no jogo. Seguindo uma história boa, divertida e ',
+            'cheia de emoções, e com ótima jogabilidade, Bully é um p*** jogo. '. 
         ].join(''),
         'full_text': [
-            'Lorem ipsum dolor sit amet, ut viverra vestibulum odio, ',
-            'aliquam in massa, velit rutrum pede ipsum mauris duis, ',
+            'o que o fazem um ',
+            'garoto bem durão, tanto com alunos quanto com a própria coordenação de ',
+            'Bullworth, se tornando uma grande dor de cabeça
             'congue massa sem elit, integer risus. Faucibus in lacus, ',
             'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
             'etiam tempus wisi semper montes odio, lorem vitae vel eu ',

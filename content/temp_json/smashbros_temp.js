af06bbd9354c240814c314cfb9a725fa31bc3c4f
BrazilGabriel <gabriel.br1994@gmail.com>
2015-04-08 14:50:14 -0400

    Update smashbros_temp.js

--- content/temp_json/smashbros_temp.js
+++ content/temp_json/smashbros_temp.js
@@ -1,80 +1,57 @@
 {
         'type': 'game', // game or console
-        'name': 'Mega Man',
-        'authors': ['Keiji Inafune'],
-        'company': ['Capcom'],  
-        'year': '1987',
-        'date': '17-12-1987', // Day, Month, Year
-        'headline': 'Dificuldade extrema!',
+        'name': 'Super Smash Bros',
+        'authors': ['Masahiro Sakurai'],
+        'company': ['Nintendo'],  
+        'year': '1999',
+        'date': '21-01-1999', // Day, Month, Year
+        'headline': 'Smash Bros',
         'resume': [
-            '<p>Mega Man (Rockman no Japão) é uma franquia criada por Keiji Inafune pela empresa Capcom. '
-            'A série é bem conhecida, tendo mais de cinquenta versões lançadas, o que a torna '
-            'a série mais produtiva da Capcom.</p>' 
-            '<p>A franquia teve início em 1987 com o primeiro Mega Man, que foi lançado para o '
-            'Nintendo Entertainment System, também conhecido como Famicom. Este jogo e suas '
-            'sequências compõe a série original de jogos, mas foram criadas várias sub-série '
-            'spinoffs que se passam no mesmo universo fictício.</p>',
-            '<p>O primeiro jogo Mega Man veio de cara trazendo a marca registrada da série: '
-            'dificuldade extrema. O jogo era tão absorvente que, mesmo a quantidade absurda de '
-            'continues, não fez os jogadores deixarem o robô de lado. Após frustrações iniciais '
-            'com o sistema de controle, dificuldade desonesta e quedas do Mega Man, quem joga '
-            'este game é recompensado com paciência e aprendizagem, principalmente através de '
-            'retrospectiva de padrões de inimigos.</p>',
-            '<p>Nem mesmo o pesadelo dos blocos que desapareciam afastou aqueles que se tornariam '
-            'fãs da saga, porque a recompensa por percorrer toda uma fase para derrotar um chefe '
-            'era gratificante: absorver a habilidade do inimigo, algo até então pouco visto nos jogos. '
-            'Gráficos e cores impecáveis, trilha sonora de primeira, tornou-se um clássico no momento '
-            'do lançamento.</p>'
+            '<p>Super Smash Bros é um jogo de luta desenvolvido pela HAL Laboratory e publicado '
+            'pela Nintendo para o Nintendo 64. Foi lançado no Japão em 21 de janeiro de 1999, '
+            'na América do Norte em 26 de abril de 1999,1 e na Europa em 19 de novembro de 1999. '
+            'É o primeiro jogo da série Super Smash Bros, seguido de Super Smash Bros - Melee '
+            'para Nintendo GameCube em 2001, Super Smash Bros - Brawl para Wii em 2008 e Super '
+            'Smash Bros para Nintendo 3DS e Wii U em 2014.</p>' 
+            '<p>O objetivo desse jogo,ao contrário de muitos jogos de luta, é arremessar o '
+            'adversário para fora da arena nos 4 cantos possíveis: direita, esquerda, para '
+            'cima ou para baixo. O dano de cada jogador é mostrado em porcentagem, quanto '
+            'maior for o dano, mais distância um golpe poderá provocar. No modo um jogador, '
+            'o jogador tem que passar por 10 desafios até chegar no chefão final o Master '
+            'Hand (o Mão Mestra).</p>'
+            '<p>Cada personagem tem a sua própria arena, cada arena é semelhante a um lugar '
+            'do jogo no qual o personagem é o protagonista. Cada arena possui um diferencial. '
+            'No modo de um jogador são feitas batalhas contra cada personagem disponível no '
+            'jogo</p>'.
+'
         ].join(''),
-        'full_text': [
-            '<p>Na série, dois gênios da robótica, Dr. Albert Wily e Dr. Thomas Light, criam '
-            'uma nova geração de robôs superior a quaisquer outros já criados (Robot Masters). '
-            'Então, Wily, ex-sócio de Dr. Light (criador de Mega Man), vendo o incrível '
-            'potencial dos robôs, começa a imaginar que, com eles sob seu comando, poderia '
-            'ter um enorme poder nas mãos. Ao compartilhar seus planos de domínio com Light '
-            'e ter retorno negativo, rouba seu projeto inicial, Protoman (Blues, no original), '
-            'para servi-lo, junto com outros robôs.</p>',
-            '<p>Dr. Light, então, cria Rock, um robô ajudante de laboratório e Roll, uma ajudante '
-            'doméstica. Quando Wily resolve usar os Robôs para cometer terrorismo, Rock não '
-            'entende por que seus irmãos estariam cometendo tais barbaridades e pede para '
-            'Dr. Thomas Light torná-lo num robô de batalha para deter Wily. Surge então Mega Man, '
-            'o robô azul que sempre estraga os planos malignos de Wily.</p>',
-            '<p>Mega Man é um jogo de plataforma clássico, side-scrolling, e um dos elementos que '
-            'chama atenção é sua jogabilidade não-linear, onde o jogador pode escolher começar '
-            'por qualquer um dos 6 estágios do jogo (a ordem que você escolhe não influencia na '
-            'história do jogo) e, ao fim de cada estágio, enfrenta um Robot Master (os chefões) e, '
-            'vencendo esse robô, ganha uma arma referente a ele que permite ao Mega usar a habilidade '
-            'do "falecido" (falecido entre aspas, porque robô não morre, vira ferro velho). Essa arma, '
-            'ao contrário do Mega Buster, tem munição limitada que pode, felizmente, ser recarregada '
-            'coletando os Weapon Energies, que são deixados por inimigos destruídos durante as fases '
-            'do game.</p>',
-            '<p>A barra de life do robozinho azul também pode ser recarregada coletando o item Life Energy. '
-            'Esse esquema de adquirir a arma do inimigo é muito legal, pois cada Robot Master possui '
-            'sua fraqueza, por exemplo: a arma que melhor funciona contra o Ice Man é o Elec Beam que '
-            'você obtém ao derrotar o Elec Man, já a fraqueza de Elec Man é a Cut Blade que você descola '
-            'ao destruir o Cut Man, e assim por diante.</p>'
-            '<p>Algumas curiosidades:</p>'
-            '<p>Os nomes de Rock e Roll são mesmo uma homenagem ao estilo musical rock'n'roll.</p>'
-            '<p>Nos Estados Unidos, o vice-presidente da Capcom Joseph Marici mudou o nome de '
-            'Rockman para Mega Man, simplesmente, pelo fato de não ter gostado do nome original japonês.</p>'
-            '<p>O nome completo de Dr. Light é Thomas Light e é uma clara homenagem a Thomas Edison e o seu '
-            'visual foi inspirado no Papai Noel. Já Dr. Wily tem como nome completo Albert Wily, uma homenagem '
-            'a Albert Einstein, inclusive seu visual foi inspirado em Einstein. O vilão foi inspirado nele, '
-            'exatamente porque Einstein tem muito a ver com a bomba atômica (que fez estragos no Japão)</p>.'
-            '<p>O visual e design do game tem muitas influências de anime e mangá, o robozinho tem o visual '
-            'fortemente influenciado pelo visual do Astro Boy. Dizem que a Capcom possuía uma licença para '
-            'lançar um jogo do Astro Boy, mas a licença expirou e a produtora resolveu utilizar o que já tinha '
-            'feito nesse jogo no game do Mega Man.</p>'
-            '<p>Originalmente, Mega Man seria vermelho, mas devido a limitação de 54 cores da paleta do Famicom, '
-            'resolveram fazê-lo azul, que era uma cor com várias tonalidades na paleta do videogame.</p>'
-            '<p>Mega Man não vendeu muito bem e Inafune considera a arte da capa americana do game como um dos '
-            'motivos para vendas tão baixas no ocidente. E, por sinal, a capa é bem bizarra: no lugar de um garoto, '
-            'um cara meio tiozinho com armadura amarela e azul segurando uma pistola e sem o Mega Buster. Com o '
-            'passar do tempo, a capa foi se tornando motivo de piada. A capa ganhou o título de capa mais feia da '
-            'história dos games pelo site GameSpy.</p>'
-            '<p>Mega Man é o único jogo da série a possuir um sistema de pontuação, e é algo totalmente inútil, '
-            'porque não há nenhuma recompensa por pontos obtidos e o cartucho não era capaz de salvar seus recordes.</p>'
-
+        'full_text': 
+            '<p>Este game fez muito sucesso no mundo inteiro, pois além de ser um dos '
+            'primeiros jogos de luta da Nintendo que envolviam personagens de diversos '
+            'games famosos, cada um deles possuía características próprias e um estilo '
+            'diferente de luta com golpes e ataques especiais totalmente incríveis, '
+            'movimentos baseados no que eles poderiam fazer em seus próprios games.</p>',
+            '<p>Mario: um dos melhores personagens para os iniciantes, pois sua '
+            'jogabilidade e bem simples, além de ser muito poderoso e poder lançar bolas '
+            'de fogo seus socos são um dos mais fortes do jogo e seu especial durante o '
+            'pulo é nostálgico, pois reparando bem nele você moedas saindo do personagem '
+            'e aquele som das moedinhas quando eram pegas em seu jogo.</p>',
+            '<p>Luigi: Luigi superou o irmão neste jogo seus combos são engraçados e seus '
+            'ataques especiais bem “criativos” e ele também pode soltar bolas de fogo das '
+            'mãos assim como Mario e também tem o som nostálgico das moedas quando o '
+            'personagem usa o ataque especial durante o pulo.</p>',
+            '<p>Yoshi: jogar com ele é complicado, mas com um tempo de treino o jogador '
+            'poderá se sair muito bem. Seus pulos são engraçados ele engole seus oponentes '
+            'e os cospe depois ou os transformam em ovos o que em alguns mapas é muito útil '
+            'para derrubar os oponentes e tirar a vida deles. Ele também pode arremessar '
+            'ovos como no jogo Yoshi Story.</p>',
+            '<p>Fox Mccloud: Fox é bem marrento sua jogabilidade é complicada seu ataque '
+            'especial durante o pulo é muito útil ainda mais se o personagem estiver caindo '
+            'nos “buracos da morte”. Ele está sempre com sua arma que lança um raio que pode '
+            'acertar os rivais até o meio da tela, e um dos poucos que tem ataques fortes '
+            'que podem acertar os oponentes de longe.</p>', 
+            '<p>Donkey Kong: Forte porem lento, Donkey Kong veio neste jogo para esbanjar '
+            'sua força, isso torna seus golpes engraçados, pois é muito desajeitado.</p>',
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},

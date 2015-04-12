6930967dd3b334eee657af3f097290ab07ece6d9
RickoBraz <marco.henrique.s.s@gmail.com>
2015-04-12 14:14:28 -0300

    Create DeadSpace_temp.js

---
+++ content/temp_json/DeadSpace_temp.js
@@ -0,0 +1,58 @@
+{
+        'type': 'Console', // game or console
+        'name': 'The Legend of Zelda',
+        'authors': ['Shigeru Miyamoto'],
+        'company': ['Nintendo'],  
+        'year': '1986',
+        'date': '21-02-1986', // Day, Month, Year
+        'headline': 'O primeiro jogo a possuir um CHIP para se salvar já incluso no game',
+        'resume': [
+            'The Legend of Zelda é um jogo single-player, ',
+            'onde você controla Link, um personagem com o objetivo de',
+            'juntar 8 peças do artefato conhecido como \“Triforce of Wisdom\”',
+            'dividido por Zelda, princesa de Hyrule',
+            'antes de ser capturada pelo antagonista, Ganon.'
+        ].join(''),
+        'full_text': [
+            'The Legend of Zelda é um jogo single-player, ',
+            'onde você controla Link, um personagem com o objetivo de',
+            'juntar 8 peças do artefato conhecido como \“Triforce of Wisdom\”',
+            'dividido por Zelda, princesa de Hyrule',
+            'antes de ser capturada pelo antagonista, Ganon.',
+            'O jogo possui uma visão de cima, na qual',
+            'se movimenta o personagem com os direcionais do controle,',
+            'tem os Botões “A” e “B”, que representavam o item que você usaria',
+            'de acordo com os itens que apareciam na parte superior da tela.',
+            'O botão \“Start\” abria o inventário.',
+            'Tendo como designer Shigeru Miyamoto,',
+            'The Legend of Zelda foi o primeiro game da Nintendo,',
+            'fora o Super Mario Bros que já vinha no console,',
+            'a ultrapassar a marca de 1 milhão de unidades vendidas na América', 
+            '(cerca de 6,5 milhões de unidades pelo mundo todo).'
+        ].join(''),
+        'thumbnail': 'thumbnail_triforce.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "Menu Principal do Game"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "Console da Nintendo"},
+            {'name': 'picture2.png', 'caption': "Console da Famicom"},
+            {'name': 'picture3.png', 'caption': "Cartucho de Edição Especial"},
+            {'name': 'picture4.png', 'caption': "Capa Americana"},
+            {'name': 'picture5.png', 'caption': "Mapa Inteiro do Jogo"},
+            {'name': 'picture6.png', 'caption': "Caixa do Board Game Criado do Jogo"},
+            {'name': 'picture7.png', 'caption': "As cartas do Board Game"},
+            {'name': 'picture8.png', 'caption': "O Mapa do Board game"},
+            {'name': 'picture9.png', 'caption': "Shigeru Miyamoto"},
+        ],
+        'video_gameplay': [
+            {'url': 'http://www.youtube.com/watch?v=RQ6hgzzk_o8','caption': "Gameplay Completo do Jogo", 'type': "youtube"},
+            {'url': 'http://www.youtube.com/watch?v=wHaZrYX0kAU','caption': "Speed Run do Jogo", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+                {'url': 'http://www.letsplaysnes.com/play-legend-of-zeldaonline-nes/','caption': "Demo On-line", 'type': "Demo"}
+        ],
+        'references': [
+            {'url': 'http://pt.slideshare.net/marcohenrique3954/the-legend-of-zelda-46649789', 'caption': "Introdução aos Jogos Eletrônicos (UEA) - Slides", 'type': 'slides'},
+            {'url': 'http://www.zelda.com.br/tloz', 'caption': "Site Oficial do Jogo"},
+            {'url': 'http://zeldawiki.org/The_Legend_of_Zelda_%28Game%29', 'caption': "Zelda Wiki"}
+        ]
+}

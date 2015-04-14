9b8eee2b75c8efd4fa79dbc69aa7d95cc1113c7f
mattizinho <matt.vieira12@hotmail.com>
2015-04-14 18:21:02 -0300

    Update Bully_temp.js

--- content/temp_json/Bully_temp.js
+++ content/temp_json/Bully_temp.js
@@ -37,20 +37,40 @@
             'O jogo começa com Jimmy deitado no carro discutindo com sua mãe e com seu padrasto. ',
             'A mãe de Jimmy quer colocá-lo num internato, enquanto ela vai passar uma longa lua de mel. ',
             'E ela escolheu Bullworth Academy, uma escola muito louca (por ser a única que aceitou Jimmy). '
-            'Chegando a escola, a mãe de Jimmy deixa-o à frente da escola, onde é recebido pela '
-            'secretária, pedindo para Jimmy ir falar com o diretor em sua sala, o Dr. Crabblesnitch.'
-            'Chegando lá, Jimmy é acusado de ser o pior aluno recebido em sua escola até agora. '
-            'Citando algumas coisas que Jimmy fez em outros internatos, como vandalismo, graffiti, '
-            'linguagem imprópria e bullying. Depois da conversa, Jimmy tem que vestir o uniforme da escola. '
-            'O jogador entrará num mundo de intrigas, fofocas, lutas e entraves que todo colégio tem. '
-            '"Bully" conseguiu capturar a essência do colegial em seu todo, onde valentões atormentam '
-            'os mais fracos e que não tem nada na cabeça. Atletas e Cheerleaders que se acham o máximo, '
-            'Nerds que vivem com a cabeça enfiada em privadas. Uma cozinheira sem higiene. '
-            'E é nesse mundo que Jimmy Hopkins é atirado e não tenta se virar do seu jeito valentão.'
-            'Como você está num internato, você terá de participar das aulas, que dão algum tipo '
-            'de prémio. Por exemplo, as aulas de inglês dão à Jimmy um vocabulário maior para se '
-            'desculpar com os monitores e colegas que querem agredi-lo;'
-            ''
+            'Chegando a escola, a mãe de Jimmy deixa-o à frente da escola, onde é recebido pela ',
+            'secretária, pedindo para Jimmy ir falar com o diretor em sua sala, o Dr. Crabblesnitch. ',
+            'Chegando lá, Jimmy é acusado de ser o pior aluno recebido em sua escola até agora. ',
+            'Citando algumas coisas que Jimmy fez em outros internatos, como vandalismo, graffiti, ',
+            'linguagem imprópria e bullying. Depois da conversa, Jimmy deve vestir uniforme da escola. ',
+            'O jogador entrará num mundo de intrigas, fofocas, lutas e entraves que todo colégio tem. ',
+            '"Bully" conseguiu capturar a essência do colegial em seu todo, onde valentões atormentam ',
+            'os mais fracos e que não tem nada na cabeça. Atletas e Cheerleaders que se acham o máximo, ',
+            'Nerds que vivem com a cabeça enfiada em privadas. Uma cozinheira sem higiene. ',
+            'E é nesse mundo que Jimmy Hopkins é atirado e não tenta se virar do seu jeito valentão. ',
+            'Como você está num internato, você terá de participar das aulas, que dão algum tipo ',
+            'de prémio. Por exemplo, as aulas de inglês dão à Jimmy um vocabulário maior para se ',
+            'desculpar com os monitores e colegas que querem agredi-lo; Aula de química que dão',
+            'a Jimmy capacidade de fazer bombinhas, bombas fedorentas, pó de mico, etc. ',
+            'As aulas são: Arte , Química , Inglês , Educação física , Fotografia e Mecânica .',
+            'Na versão "Scholarship Edition", de PC, Wii e Xbox 360, existem mais missões e aulas, ',
+            'como: Música, Biologia, Matemática e Geografia.',
+            'Bully não tem um objetivo linear, dando liberdade para o jogador fazer o que quiser, ',
+            'na hora que quiser e quando bem entender. Tendo também a influência do tempo durante ',
+            'o jogo, como, estando a noite, em horário de aula ou de manha cedo, dando ao jogo',
+            'perspectivas diferentes. Tudo faz com que a história do jogo seja muito divertida. ',
+            'Jimmy pode também navegar através das seis facções da escola: Os bullies, Nerds, ',
+            'Preppies, Greasers, Jocks e Townies. A maioria das facções possuem apenas um membro ',
+            'do sexo feminino, a não ser a dos Bullies que não possuem nenhuma. Com qualquer ',
+            'uma dessas meninas Jimmy pode ter um caso. No jogo, diferente dos demais da ',
+            'produtora Rockstar, há também personagens que não são de facções, porém tem suas ',
+            'singularidades. São eles: Trevor, Gordon, Ivan, Konstantinos, Ray, Lance, Angie, ',
+            'Eunice e Christie, com quem Jimmy também pode interagir.',
+            'As opiniões dos críticos em referência ao jogo Bully geralmente foram positivas. ',
+            'A IGN o avaliou como 8.9/10, a Games Radar lançou 9.0/10, um grande 10/10 da 1Up.com ',
+            '8.7/10 da Gamespot, 35/40 da Famitsu, um grande 5/5 da Jive Magazine, deixando Bully ',
+            'como um dos 10 melhores jogos de Playstation 2. Com certeza Bully é um jogo ',
+            'aprovado pelo público e pelos críticos, mesmo sendo polêmico, afinal é da ',
+            'Rockstar. Jogo super recomendado!'.
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},
@@ -62,7 +82,8 @@
         'video_gameplay': [
             {'url': 'video_url', 'caption': "description", 'type': "youtube"} // type: youtube or vimeo
         ],
-        'demos': [
+        'links_uteis': [
+            {'url': 'demonstration_url', 'caption': "description"}
             {'url': 'demonstration_url', 'caption': "description"}
         ],
         'references': [

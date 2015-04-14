eb7071d0301950568e3090bfb92df7b5d0d175e4
mattizinho <matt.vieira12@hotmail.com>
2015-04-14 17:48:38 -0300

    Update Bully_temp.js

--- content/temp_json/Bully_temp.js
+++ content/temp_json/Bully_temp.js
@@ -25,18 +25,32 @@
             'cheia de emoções, e com ótima jogabilidade, Bully é um p*** jogo. '. 
         ].join(''),
         'full_text': [
-            'o que o fazem um ',
-            'garoto bem durão, tanto com alunos quanto com a própria coordenação de ',
-            'Bullworth, se tornando uma grande dor de cabeça
-            'congue massa sem elit, integer risus. Faucibus in lacus, ',
-            'ligula ornare nibh, elit suspendisse tortor sed volutpat, ',
-            'etiam tempus wisi semper montes odio, lorem vitae vel eu ',
-            'convallis. Volutpat blandit egestas et libero eu per, venenatis ',
-            'aenean, proin quis aliquam viverra potenti vestibulum auctor, ',
-            'non amet tellus sodales. In ullamco tellus risus nisl morbi amet. ',
-            'Vel quisque nunc in placerat. Vitae id urna, nulla suspendisse, ',
-            'molestie sodales ac nisl nulla, tempus occaecati. Sodales lectus, ',
-            'mauris dis lectus amet, nascetur ipsum, hac suspendisse blandit eros.'
+            'Bully, que também é conhecido como Cão Come Cão na América Latina,é um jogo ',
+            'eletrônico de ação-aventura em terceira pessoa, lançado pela Rockstar Vancouver ',
+            'para Playstation 2 em 17 de outubro de 2006, na América do Norte, e em 25 de outubro '
+            'de 2006 no Reino Unido. Uma versão de Xbox foi planejada, porém acabou sendo cancelada ',
+            'por motivos não-revelados. O jogo foi relançado como Bully: Scholarship Edition ',
+            'em 4 de março de 2008 para o Wii e o Xbox 360, e em 24 de outubro de 2008 para o PC. ',
+            'A versão do PlayStation 2 do jogo também está disponível nos EUA como uma edição especial, ',
+            'que inclui uma revista em quadrinhos de edição limitada e uma bola de queimada igual ',
+            'a que aparece no jogo, com o título do jogo gravado nela. ',
+            'O jogo começa com Jimmy deitado no carro discutindo com sua mãe e com seu padrasto. ',
+            'A mãe de Jimmy quer colocá-lo num internato, enquanto ela vai passar uma longa lua de mel. ',
+            'E ela escolheu Bullworth Academy, uma escola muito louca (por ser a única que aceitou Jimmy). '
+            'Chegando a escola, a mãe de Jimmy deixa-o à frente da escola, onde é recebido pela '
+            'secretária, pedindo para Jimmy ir falar com o diretor em sua sala, o Dr. Crabblesnitch.'
+            'Chegando lá, Jimmy é acusado de ser o pior aluno recebido em sua escola até agora. '
+            'Citando algumas coisas que Jimmy fez em outros internatos, como vandalismo, graffiti, '
+            'linguagem imprópria e bullying. Depois da conversa, Jimmy tem que vestir o uniforme da escola. '
+            'O jogador entrará num mundo de intrigas, fofocas, lutas e entraves que todo colégio tem. '
+            '"Bully" conseguiu capturar a essência do colegial em seu todo, onde valentões atormentam '
+            'os mais fracos e que não tem nada na cabeça. Atletas e Cheerleaders que se acham o máximo, '
+            'Nerds que vivem com a cabeça enfiada em privadas. Uma cozinheira sem higiene. '
+            'E é nesse mundo que Jimmy Hopkins é atirado e não tenta se virar do seu jeito valentão.'
+            'Como você está num internato, você terá de participar das aulas, que dão algum tipo '
+            'de prémio. Por exemplo, as aulas de inglês dão à Jimmy um vocabulário maior para se '
+            'desculpar com os monitores e colegas que querem agredi-lo;'
+            ''
         ].join(''),
         'thumbnail': 'thumbnail_example.png',
         'main_img': {'name': 'main_picture.png', 'caption': "description"},

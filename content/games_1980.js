df4b0b78a8daf14386a8fa19482e49cfbc687e8a
Rodrigo Braga <rodrigogrow@gmail.com>
2015-04-26 10:49:12 -0400

    add frogger

--- content/games_1980.js
+++ content/games_1980.js
@@ -9,6 +9,73 @@
 
 		'decade': '1980',
 		'games': [
+            {
+                'type': 'game', // game or console
+                'name': 'Frogger',
+                'authors': [],
+                'company': ['Konami/SEGA'],  
+                'year': '1981',
+                'headline': 'Frogger',
+                'resume': [
+                    'O título foi lançado pela Konami em 1981 para os arcades ',
+                    'e um ano depois, em 1982, foi lançado para o Atari 2600 ',
+                    'com algumas pequenas alterações gráficas devido às limitações ',
+                    'de cada plataforma. O objetivo do jogo era muito simples: ',
+                    'o jogador controlava um sapo, movendo-se para os lados, frente e trás ',
+                    'que precisa atravessar uma rua repleta de veículos e em seguida ',
+                    'um riacho, passando por cima de troncos que boiavam. Se o sapo ',
+                    'fosse atropelado por qualquer veículo ou caísse na água, Acabava o jogo.'
+                ].join(''),
+                'full_text': [
+                    'Frogger foi um jogo criado em 1981 em uma parceria entre as então jovens ',
+                    'empresas de informática SEGA e Konami. Inicialmente fabricado para ',
+                    'máquinas de arcade, logo foi fabricado em grande escala para o console ',
+                    'mais popular da época, o Atari 2600, tornando-se também  um de seus jogos ',
+                    'mais jogados. A mecânica do jogo era simples, onde o jogador controla um sapo ',
+                    'podendo ser movimentado para os lados, para frente e para trás. E a cada ',
+                    'cinco anfíbios que o jogador conseguisse colocar do outro lado do rio, ',
+                    'um novo nível era iniciado. A dificuldade de cada fase era medida pela ',
+                    'velocidade dos objetos na avenida e no rio, deixando tudo bastante complicado ',
+                    'depois de alguns níveis. Frequentemente, moscas e outros sapos apareciam ',
+                    'durante o percurso, e se o jogador conseguisse pegá-lo, ganhava pontos. ',
+                    'Frogger foi um fenômeno dos videogames nos arcades e no Atari, ',
+                    'além de ter sido o primeiro grande hit da Konami. ',
+                    'Uma curiosidade de frogger é que aparece também em um seriado Americano, ',
+                    'chamado Seinfield, quando George Constanza, um dos personagens, cruza ',
+                    'a cidade com um fliperama original de Frogger, parodiando a própria ',
+                    'travessia de avenidas do título, Constanza fez um recorde ficcional de ',
+                    '863,050 pontos. A única pontuação real registrada que supera a de Seinfield ',
+                    'é de Michael Smith, de Springfield, em 2012, com 970,440 de pontos.',
+                    'Superando o norte-americano Pat Laffaye, com 896,980 pontos. Além disso, ',
+                    'o Frogger original, ou conversões similares da versão arcade, foi lançado ',
+                    'para mais de 10 plataformas, desde as mais conhecidas às mais raras. ',
+                    'Consoles como o Playstation ganharam uma versão 3D, que retomava ',
+                    'o jogo original e incluía novos níveis para controlar o sapinho. ',
+                    'No Super NES, Frogger foi um dos últimos jogos a ser lançados. ',
+                    'Entre os consoles atuais, somente o Xbox 360 disponibiliza-o para baixar.'
+                ].join(''),
+                'thumbnail': 'thumbnail_example.png',
+                'main_img': {'name': 'main_image.jpg', 'caption': "Arcade Frogger!"},
+                'images': [
+                    {'name': 'picture_1.jpg', 'caption': "Logo do jogo Frogger"},
+                    {'name': 'picture_2.png', 'caption': "Gameplay para Arcade"},
+                    {'name': 'picture_3.png', 'caption': "Gameplay para Atari 2600"},
+                    {'name': 'picture_4.jpg', 'caption': "Capa do Frogger pra XBOX"}
+                ],
+                'video_gameplay': [
+                    {'url': 'http://www.youtu.be/l9fO-YuWPSk', 'caption': "Frogger arcade game", 'type': "youtube"}, // type: youtube or vimeo
+                    {'url': 'http://www.youtu.be/vEsq-PGRJ5I', 'caption': "Frogger Atari 2600", 'type': "youtube"} // type: youtube or vimeo
+                ],
+                'demos': [
+                    {'url': 'http://www.ojogos.com.br/jogo/frogger', 'caption': "Jogar frogger!"}
+                ],
+                'references': [
+                    {'url': 'http://pt.slideshare.net/mattizinho/jogo-frogger', 'caption': "Introdução aos Jogos Eletrônicos (UEA) - Slides", 'type': 'slides'},
+                    {'url': 'https://baudovideogame.wordpress.com/2010/05/04/frogger-atari/', 'caption': "Frogger, seu legado e história"},
+                    {'url': 'http://contemgames.com.br/historia/jogos/1981-Frogger.aspx', 'caption': "História do jogo Frogger"},
+                    {'url': 'http://en.wikipedia.org/wiki/Frogger', 'caption': "Frogger Wiki"}
+                ]
+            },
     		{
                 'type': 'game', // game or console
                 'name': 'Street Fighter',

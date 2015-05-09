1ce470cb50a841ebcdc134c366ef2a2d20c9534b
Rodrigo Braga <rodrigogrow@gmail.com>
2015-05-09 18:52:31 -0400

    add donkey kong entry

--- content/games_1980.js
+++ content/games_1980.js
@@ -78,6 +78,78 @@
             },
             {
                 'type': 'game', // game or console
+                'name': 'Donkey Kong',
+                'authors': ['Shigeru Miyamoto','Gunpei Yokoi','Hiroshi Yamauchi'],
+                'company': ['Nintendo','Ikegami Tsushinki'],  
+                'year': '1981',
+                'date': '09-07-1981', // Day, Month, Year
+                'headline': 'O jogo que definiu o gênero Plataforma e originou Mario e Donkey Kong',
+                'resume':   [
+                    'Donkey Kong é um jogo de Arcade desenvolvido pela Nintendo e um dos primeiros',
+                    'exemplos do gênero plataforma, já que a jogabilidade consiste em levar o personagem ',
+                    'através de plataformas, enquanto se esquiva de obstáculos. ',
+                    'O jogo foi mais um na série de tentativas realizadas pela Nintendo, com o objetivo',
+                    'de ganhar o mercado dos EUA. Hiroshi Yamauchi, presidente da Nintendo na época, delegou ',
+                    'a Shigeru Miyamoto a tarefa de produzir um jogo capaz de ganhar o mercado em questão, ',
+                    'sob a inspeção e direção de Gunpei Yokoi. ',
+                    'Apesar das dúvidas internas em relação ao projeto no começo, Donkey Kong foi um sucesso, ',
+                    'tanto de vendas quanto de críticas, garantindo o ingresso da Nintendo nos EUA.'
+                ].join(''),
+                'full_text': [
+                    'Ao final de 1980, toas as tentativas da Konami de entrar definitivamente no mercado dos EUA haviam  ',
+                    'fracassado, a última sendo com Radar Scope. Como resultado, a empresa ficou com várias máquinas de  ',
+                    'Radar Scope não vendidas. Foi então que Hiroshi Yamauchi, presidente da Nintendo, teve uma conversa ',
+                    'com Shigeru Miyamoto, que já trabalhava para a Nintendo desde 1977, e o perguntou se ele seria capaz ',
+                    'de converter as máquinas de Radar Scope excedentes em algo que ganharia o mercado dos EUA. Miyamoto  ',
+                    'aceitou a tarefa, sendo supervisionado por Gunpei Yokoi. ',
+                    'Miyamoto pretendia criar uma narrativa simples, mas com personagens "coloridos" e carismáticos. Para ',
+                    'isso, buscou inspiração em "Popeye", no romance "A Bela e a Fera" e no filme "King Kong". O resultado',
+                    'do trabalho de Miyamoto no projeto foram os personagens Jumpman(posteriormente chamado de Mario), o  ',
+                    'gorila Donkey Kong, que seria uma espécie de animal de estimação de Jumpman e que, por algum motivo ',
+                    '(pode ser que Jumpman tenha o ignorado para passar mais tempo com sua namorada), rapta a dama Pauline,',
+                    'namorada de Jumpman. ',
+                    'O jogo, então, consiste em controlar Jumpman e levá-lo até o topo de um prédio em construção, para onde ',
+                    'Donkey Kong correu levando Pauline, evitando ser atingido pelos barris lançados do topo pelo gorila, além ',
+                    'de outros obstáculos presentes devivo ao fato de o local ainda estar em construção. E assim o projeto foi',
+                    'tomando forma: Miyamoto idealizava os aspectos do jogo e levava suas ideias para uma equipe de outra empresa,  ',
+                    'Ikegami Tsushinki, que era responsável por programar tudo o que Miyamoto apresentava. Além disso, o jogo ',
+                    'recebeu uma trilha sonora simples, composta por Yukio Kaneoka. ',
+                    'Hiroshi Yamauchi estava confiante no sucesso de Donkey Kong, e apresentou o projeto a Minoru Arakawa, líder ',
+                    'das operações da Nintendo nos EUA. Ron Judy e Al Stone, responsáveis pela distribuição do jogo nos EUA cuidaram ',
+                    'para que o projeto fosse devidamente regristrado. O jogo começou a ser traduzido para o inglês e os personagens',
+                    'receberam nomes mais apropriados. Jumpman passou a ser chamado de Mario e sua namorada, até então conhecida ',
+                    'somente como Lady, foi batizada como Pauline. O nome do gorila foi mantido e adotado como nome também para o jogo. ',
+                    'Donkey Kong foi oficialmente lançado no dia 9 de Julho de 1981 e foi um sucesso gigantesco nos EUA e Canada. As  ',
+                    '2000 unidades disponíveis no início foram rapidamente vendidas e, em Outubro, uma média de 4.000 unidades por mês ',
+                    'haviam sido vendidas. Ao fim de Junho de 1982, a Nintendo já havia vendido mais do que 60.000 máquinas do jogo.',
+                    'Além de todo o sucesso monetário do jogo, ele foi muito importante não só para a Nintendo, mas para o universo   ',
+                    'dos jogos eletrônicos em geral, dando origem às franquias Mario e Donkey Kong, além de apresentar o talento de ',
+                    'Shigeru Miyamoto ao mundo. '
+                ].join(''),
+                'thumbnail': 'dk_thumbnail.png',
+                'main_img': {'name': 'dk_main.png', 'caption': "Capa NES"},
+                'images': [
+                    {'name': 'dk_pic1.png', 'caption': "25m - Primeiro nível do jogo"},
+                    {'name': 'dk_pic2.png', 'caption': "50m - Segundo nível do jogo"},
+                    {'name': 'dk_pic3.png', 'caption': "75m - Terceiro nível do jogo"},
+                    {'name': 'dk_pic4.png', 'caption': "100m - Quarto e último nível do jogo"},
+                    {'name': 'dk_pic5.png', 'caption': "Arte que mostra os personagens do jogo"}
+                ],
+                'video_gameplay': [
+                    {'url': 'http://youtu.be/Pp2aMs38ERY', 'caption': "Arcade: Donkey Kong (1981) Gameplay", 'type': "youtube"} // type: youtube or vimeo
+                ],
+                'demos': [
+                    {'url': 'http://www.mariodonkeykong.com/full-203-Donkey-Kong.html', 'caption': "Gameplay completa, fullscreen"},
+                    {'url': 'http://www.classicgaming.cc/classics/donkeykong/game_flash.php', 'caption': "Gameplay completa."}
+                ],
+                'references': [
+                    {'url': 'http://pt.wikipedia.org/wiki/Donkey_Kong', 'caption': "Wikipedia (PT)"},
+                    {'url': 'http://en.wikipedia.org/wiki/Donkey_Kong_(video_game)', 'caption': "Wikipedia (EN)"},
+                    {'url': 'https://youtu.be/tJagEKVJ8x4', 'caption': "Vídeo mostrando um Arcade de Donkey Kong funcionando"}
+                ]
+            },
+            {
+                'type': 'game', // game or console
                 'name': 'Pitfall',
                 'authors': ['David Crane'],
                 'company': ['Activision'],  
